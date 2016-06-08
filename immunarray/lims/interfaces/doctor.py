cf902596010efaf4e4d8bd19c859c928fa6af959
immunarray <justin.pitts@immunarray.com>
2016-06-08 10:31:21 -0400

    add needed fields for insurance billing

--- immunarray/lims/interfaces/doctor.py
+++ immunarray/lims/interfaces/doctor.py
@@ -13,7 +13,6 @@ class IDoctorFolder(Interface):
 
 class IDoctor(IPerson):
     """
-    """
     Firstname = schema.ASCIILine(
         title=_(u"Firstname"),
         description=_(u""),
@@ -32,5 +31,36 @@ class IDoctor(IPerson):
     Phone = schema.ASCIILine(
         title=_(u"Phone number"),
         description=_(u""),
+        required=False,
+    )
+    """
+    NPI = schema.ASCIILine(
+        title=_(u"NPI"),
+        description=_(u"NPI (unique to each provider)"),
+        required=True,
+    )
+    Credentials = schema.ASCIILine(
+        title=_(u"Provider Credentials"),
+        description=_(u"Provider credentials (M.D.  D.O.)"),
+        required=False,
+    )
+    TaxID = schema.ASCIILine(
+        title=_(u"Tax ID"),
+        description=_(u"Tax ID"),
+        required=True,
+    )
+    PIN = schema.ASCIILine(
+        title=_(u"PIN"),
+        description=_(u"PIN"),
+        required=True,
+    )
+    UPIN = schema.ASCIILine(
+        title=_(u"UPIN"),
+        description=_(u"UPIN"),
+        required=True,
+    )
+    Fax = schema.ASCIILine(
+        title=_(u"Fax Number"),
+        description=_(u"Fax number for result delivery"),
         required=False,
     )
