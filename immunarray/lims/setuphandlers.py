e6888a2478110839e56271d6e825522aeed2e2ad
Campbell <campbell@bikalabs.com>
2016-06-13 15:17:57 +0200

    setuphandlers for creating custom materials (done!)

--- immunarray/lims/setuphandlers.py
+++ immunarray/lims/setuphandlers.py
@@ -9,6 +9,32 @@ from plone.dexterity.fti import DexterityFTI
 from zope.interface import alsoProvides
 from zope.lifecycleevent import modified
 
+def create_fti(portal, id, title, schema):
+    fti = DexterityFTI(id)
+    fti.id = id
+
+    fti.manage_changeProperties(
+        factory=id,
+        title=title.encode('utf8'),
+        description=u"Type information for %s" % title.encode(),
+        i18n_domain='immunarray.lims',
+        klass='immunarray.lims.interfaces.material.IMaterial',
+        model_source=schema,
+        immediate_view='folder_contents',
+        icon_expr='string:document_icon.png',
+        # add_view_expr='string:${folder_url}/++add+' + id,
+        filter_content_types=True,
+        allowed_content_types=[],
+        global_allow=True,
+        behaviors=[
+            "immunarray.lims.interfaces.material.IMaterial",
+        ],
+        add_permission='immunarray.lims.permissions.AddMaterial',
+    )
+    if id in portal.portal_types:
+        del portal.portal_types[id]
+    portal.portal_types._setObject(id, fti)
+
 def setupMaterials(context):
     """Configure a set of Materials types.  In normal operation, these can be
     defined direcly by adding new Dexterity types and applying the IMaterial
@@ -27,70 +53,63 @@ def setupMaterials(context):
                                     title='Materials')
         modified(portal.materials)
 
-        # Restrict users who may add materials
-        mp = portal.materials.manage_permission
-        mp(AddMaterial, ['Manager', 'Owner'], 0)
+        # Remove default allowed content types from materials folder
+        # folder.setConstrainTypesMode(1)
+        # folder.setLocallyAllowedTypes(("Collection", "File",))
+        # folder.setImmediatelyAddableTypes(("Collection", "File",))
+        # from Products.CMFPlone.interfaces import IConstrainTypes
+        # folder = IConstrainTypes(folder)
+        # folder.setLocallyAllowedTypes([])
+        # folder.setImmediatelyAddableTypes([])
 
     # built-in materials
     materials = [
-        ('caseinsalt', _(u"Casein Salt"), """<model xmlns:lingua="http://namespaces.plone.org/supermodel/lingua"
-             xmlns:users="http://namespaces.plone.org/supermodel/users"
-             xmlns:security="http://namespaces.plone.org/supermodel/security"
-             xmlns:marshal="http://namespaces.plone.org/supermodel/marshal"
-             xmlns:form="http://namespaces.plone.org/supermodel/form"
-             xmlns="http://namespaces.plone.org/supermodel/schema">
-          <schema>
-            <field name="randomfield" type="zope.schema.Choice">
-              <description>This field is available in casein salt materials only.</description>
-              <title>Casine Salt Random Field</title>
-              <values>
-                <element>42</element>
-                <element>111</element>
-                <element>256</element>
-              </values>
-            </field>
-          </schema>
-        </model>"""),
-        ('ethylalcoholdenatured', _(u"Ethylalcahol Denatured"),
-         """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
-        ('glycerol', _(u"Glycerol"),
-         """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
-        ('kcl', _(u"Potassium Chloride (KCl)"),
-         """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
-        ('kcl', _(u"Potassium Chloride (KCl)"),
-         """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
-        ('kh2po4', _(u"Potassium Phosphatemonobasic (KH2PO4)"),
-         """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        # ('caseinsalt', _(u"Casein Salt"), """<model xmlns:lingua="http://namespaces.plone.org/supermodel/lingua"
+        #      xmlns:users="http://namespaces.plone.org/supermodel/users"
+        #      xmlns:security="http://namespaces.plone.org/supermodel/security"
+        #      xmlns:marshal="http://namespaces.plone.org/supermodel/marshal"
+        #      xmlns:form="http://namespaces.plone.org/supermodel/form"
+        #      xmlns="http://namespaces.plone.org/supermodel/schema">
+        #   <schema>
+        #     <field name="randomfield" type="zope.schema.Choice">
+        #       <description>This field is available in casein salt materials only.</description>
+        #       <title>Casine Salt Random Field</title>
+        #       <values>
+        #         <element>42</element>
+        #         <element>111</element>
+        #         <element>256</element>
+        #       </values>
+        #     </field>
+        #   </schema>
+        # </model>"""),
+        ("caseinsalt", _(u"Casein Salt"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        ("ethylalcohol", _(u"Ethyl Alcahol Denatured"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        ("glycerol", _(u"Glycerol"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        ("nacl", _(u"Sodium Chloride (NaCl)"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        ("kcl", _(u"Potassium Chloride (KCl)"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        ("kh2po4", _(u"Potassium Phosphatemonobasic (KH2PO4)"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        ("na2hpo4", _(u"Sodium Phosphatedibasic (Na2HPO4)"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        ("naoh", _(u"Sodium Hydroxide 2.5N (NaOH)"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        ("tween20", _(u"Tween 20"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        ("hcl37", _(u"Hydrochloricacid 37%"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        ("iggcy3", _(u"IgG-Cy3"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
+        ("igmaf647", _(u"IgM-AF647"), """<model xmlns="http://namespaces.plone.org/supermodel/schema"><schema></schema></model>"""),
     ]
 
     for id, title, schema in materials:
-        fti = DexterityFTI(id)
-        fti.id = id
-
-        fti.manage_changeProperties(
-            factory=id,
-            title=title.encode('utf8'),
-            description=u"Type information for %s" % title.encode(),
-            i18n_domain='immunarray.lims',
-            klass='immunarray.lims.interfaces.material.IMaterial',
-            model_source=schema,
-            immediate_view='folder_contents',
-            icon_expr='string:document_icon.png',
-            # add_view_expr='string:${folder_url}/++add+' + id,
-            filter_content_types=True,
-            allowed_content_types=[],
-            global_allow=True,
-            behaviors=[
-                "immunarray.lims.interfaces.material.IMaterial",
-            ],
-            add_permission='immunarray.lims.permissions.AddMaterial',
-        )
-        if id in portal.portal_types:
-            del portal.portal_types[id]
-        portal.portal_types._setObject(id, fti)
+        create_fti(portal, id, title, schema)
 
     return "Created material types"
 
+
+def setupPermissions(context):
+    if context.readDataFile('immunarray.lims.txt') is None:
+        return
+
+    portal = context.getSite()
+
+    mp = portal.materials.manage_permission
+    mp(AddMaterial, ['Manager', 'Owner'], 0)
 
 def setupVarious(context):
     if context.readDataFile('immunarray.lims.txt') is None:
@@ -98,7 +117,3 @@ def setupVarious(context):
 
     portal = context.getSite()
 
-    # Restrict users who may add Materials (nobody!)
-    # the materials folder has different permissions.
-    mp = portal.manage_permission
-    mp(AddMaterial, [], 0)
