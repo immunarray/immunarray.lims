800872399ae1c1c54f6596ee239cb2388ea29998
immunarray <justin.pitts@immunarray.com>
2016-09-06 13:54:57 -0400

    removed spaces, clean formatting.

--- immunarray/lims/interfaces/nce.py
+++ immunarray/lims/interfaces/nce.py
@@ -16,20 +16,20 @@ class INCE(model.Schema):
     """
 
     tracking_number = schema.TextLine(
-        title = _(u"Tracking Number"),
-        description = _(u"Tracking Number"),
+        title=_(u"Tracking Number"),
+        description=_(u"Tracking Number"),
         required=False,
     )
 
     Date_of_NCE = schema.Date(
-        title = _(u"Date of NCE"),
-        description = _(u"Date of NCE"),
+        title=_(u"Date of NCE"),
+        description=_(u"Date of NCE"),
         required=True,
     )
 
     date_of_nce_discovery = schema.Date(
-        title = _(u"Date of NCE Discovery"),
-        description = _(u"Date of NCE Discovery"),
+        title=_(u"Date of NCE Discovery"),
+        description=_(u"Date of NCE Discovery"),
         required=True,
     )
     
@@ -41,33 +41,33 @@ class INCE(model.Schema):
     )
 
     position = schema.TextLine(
-        title = _(u"Position of Reporter"),
-        description = _(u"Position of Reporter"),
+        title=_(u"Position of Reporter"),
+        description=_(u"Position of Reporter"),
         required=False,
     )
 
     area_of_operation = schema.Choice(
-        title = _(u"Area of Operation"),
-        description = _(u"Area of Operation"),
+        title=_(u"Area of Operation"),
+        description=_(u"Area of Operation"),
         values=[_(u'Scanning'),_(u'Accessioning'),_(u'Test Preparation'),_(u'Blocking'),_(u'Testing'),_(u'Data Analysis')],
         required=False,
     )
 
     accession_number = schema.TextLine(
-        title = _(u"Accession Number or Batch Number"),
-        description = _(u"Accession Number or Batch Number"),
+        title=_(u"Accession Number or Batch Number"),
+        description=_(u"Accession Number or Batch Number"),
         required=False,
     )
 
     assay = schema.TextLine(
-        title = _(u"Assay"),
+        title=_(u"Assay"),
         description=_(u"Assay"),
         required=False,
     )
 
     facility = schema.TextLine(
-        title = _(u"Facility where NCE Happened"),
-        description = _(u"Facility where NCE Happened"),
+        title=_(u"Facility where NCE Happened"),
+        description=_(u"Facility where NCE Happened"),
         required=False,
     )
 
@@ -97,63 +97,63 @@ class INCE(model.Schema):
         required=False,
     )
     initial_finding = schema.Text(
-        title = _(u"Initial Finding"),
-        description = _(u"Initial Finding"),
+        title=_(u"Initial Finding"),
+        description=_(u"Initial Finding"),
         required=False,
     )
 
     first_occurrence = schema.Bool(
-        title = _(u"First Occurrence"),
-        description = _(u"First Occurrence"),
-        required = True,
-        default = False,
+        title=_(u"First Occurrence"),
+        description=_(u"First Occurrence"),
+        required=True,
+        default=False,
     )
 
     persons_involved = schema.TextLine(
-        title = _(u"Persons Involved"),
-        description = _(u"Persons Involved"),
-        required = False,
+        title=_(u"Persons Involved"),
+        description=_(u"Persons Involved"),
+        required=False,
     )
 
     remedial_action_type = schema.Choice(
-        title = _(u"Remedial Action Type"),
-        description = _(u"Remedial Action Type"),
-        values = [_(u'Corrective'),_(u'Preventive'),_(u'Process Improve')],
-        required = False,
+        title=_(u"Remedial Action Type"),
+        description=_(u"Remedial Action Type"),
+        values=[_(u'Corrective'),_(u'Preventive'),_(u'Process Improve')],
+        required=False,
     )
 
     remedial_action = schema.Text(
-        title = _(u"Remedial Action"),
-        description = _(u"Remedial Action"),
-        required = False,
+        title=_(u"Remedial Action"),
+        description=_(u"Remedial Action"),
+        required=False,
     )
     is_follow_up_needed = schema.Bool(
-        title = _(u"Is Follow Up Needed?"),
-        description = _(u"Is Follow Up Needed?"),
-        required = True,
-        default = False,
+        title=_(u"Is Follow Up Needed?"),
+        description=_(u"Is Follow Up Needed?"),
+        required=True,
+        default=False,
     )
 
     investigation_needed = schema.Bool(
-        title = _(u"Investigation Needed"),
-        description = _(u"Investigation Needed"),
-        required = True,
-        default = False,
+        title=_(u"Investigation Needed"),
+        description=_(u"Investigation Needed"),
+        required=True,
+        default=False,
     )
 
     Investigation_tracking_number = schema.TextLine(
-        title = _(u"Investigation Tracking Number"),
-        description = _(u"Investigation Tracking Number"),
+        title=_(u"Investigation Tracking Number"),
+        description=_(u"Investigation Tracking Number"),
         required=False,
     )
     nce_status = schema.Choice(
-        title = _(u"Status"),
-        description = _(u"Status"),
-        values = [_(u'Closed'),_(u'In Process'),_(u'Non Resolvable')],
-        required = False,
+        title=_(u"Status"),
+        description=_(u"Status"),
+        values=[_(u'Closed'),_(u'In Process'),_(u'Non Resolvable')],
+        required=False,
     )
     date_closed = schema.Date(
-        title = _(u"Date NCE Closed"),
-        description = _(u"Date NCE Closed"),
-        required = False,
+        title=_(u"Date NCE Closed"),
+        description=_(u"Date NCE Closed"),
+        required=False,
     )
