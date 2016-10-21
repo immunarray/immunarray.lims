53a96b7249ee1a2b3663e7aebfdfe1045418789e
immunarray <justin.pitts@immunarray.com>
2016-10-20 21:01:41 -0400

    python portiong for ImmnuArray Control Panel

---
+++ immunarray/lims/browser/controlpanel.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.z3cform import layout
+from zope import schema
+from zope.interface import Interface
+
+
+class IImmunArrayControlPanel(Interface):
+    pass
+
