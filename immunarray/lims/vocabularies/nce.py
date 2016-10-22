f96bc8000414f8fde515f64b1e4d5ea54f9546ae
immunarray <justin.pitts@immunarray.com>
2016-10-22 16:40:16 -0400

    change so that the typed name displayed not the name in the catalog

--- immunarray/lims/vocabularies/nce.py
+++ immunarray/lims/vocabularies/nce.py
@@ -12,7 +12,7 @@ class PrimaryNCE(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('immunarray.category_primary')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(normalizer.normalize(i), i) for i in values]
+        items = [(i, normalizer.normalize(i)) for i in values]
         return SimpleVocabulary.fromItems(items)
 
 PrimaryNCEVocabulary = PrimaryNCE()
@@ -23,7 +23,7 @@ class SecondaryNCE(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('immunarray.category_secondary')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(normalizer.normalize(i), i) for i in values]
+        items = [(i, normalizer.normalize(i)) for i in values]
         return SimpleVocabulary.fromItems(items)
 
 SecondaryNCEVocabulary = SecondaryNCE()
@@ -34,7 +34,7 @@ class TertiaryNCE(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('immunarray.category_tertiary')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(normalizer.normalize(i), i) for i in values]
+        items = [(i, normalizer.normalize(i)) for i in values]
         return SimpleVocabulary.fromItems(items)
 
 TertiaryNCEVocabulary = TertiaryNCE()
