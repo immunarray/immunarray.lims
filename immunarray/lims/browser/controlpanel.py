a505d56d070c70913c51e174b62c0be7a929ac93
immunarray <justin.pitts@immunarray.com>
2016-10-22 14:03:15 -0400

    creations of Immunarray Control Panel

--- immunarray/lims/browser/controlpanel.py
+++ immunarray/lims/browser/controlpanel.py
@@ -8,5 +8,41 @@ from zope.interface import Interface
 
 
 class IImmunArrayControlPanel(Interface):
-    pass
+
+    category_primary = schema.Tuple(
+        title=u'NCE Parimary Category',
+        default=(u'Process Management', u'Purchasing and Inventory',
+                 u'Equipment'),
+        missing_value=None,
+        required=False,
+        value_type=schema.TextLine()
+    )
+
+    category_secondary = schema.Tuple(
+        title=u'NCE Secondary Category',
+        default=(u'Scanning', u'Accessioning', u'Test Preparation',
+                 u'Blocking', u'Testing', u'Data Analysis'),
+        missing_value=None,
+        required=False,
+        value_type=schema.TextLine()
+    )
+
+    category_tertiary = schema.Tuple(
+        title=u'NCE Tertiary Category',
+        default=(u'Scanning', u'Accessioning', u'Test Preparation',
+                 u'Blocking', u'Testing', u'Data Analysis'),
+        missing_value=None,
+        required=False,
+        value_type=schema.TextLine()
+    )
+
+
+class ImmunArrayControlPanelForm(RegistryEditForm):
+    schema = IImmunArrayControlPanel
+    schema_prefix = "immunarray"
+    label = u'ImmunArray Control Panel Settings'
+
+
+ImmunArrayControlPanelView = layout.wrap_form(
+    ImmunArrayControlPanelForm, ControlPanelFormWrapper)
 
