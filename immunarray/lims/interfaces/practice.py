d0e9e84b9cbf1122b1f81246929a075bf85bf226
immunarray <justin.pitts@immunarray.com>
2016-10-22 17:16:13 -0400

    addition of fields and changed contact name to be one line, it should not be used in a search need, just as a who to call need

--- immunarray/lims/interfaces/practice.py
+++ immunarray/lims/interfaces/practice.py
@@ -6,14 +6,9 @@ from zope import schema
 class IPractice(IOrganisation):
     """Base fields for all organisation types
     """
-    contact_first_name = schema.TextLine(
+    contact_name = schema.TextLine(
         title=_(u"First name of primary pratice contact"),
         description=_(u"First name of primary pratice contact"),
-        required=False,
-    )
-    contact_last_name = schema.TextLine(
-        title=_(u"Last name of primay practice contact"),
-        description=_(u"Last name of primay practice contact"),
         required=False,
     )
     therapak_id = schema.Int(
@@ -25,4 +20,19 @@ class IPractice(IOrganisation):
         title = _(u"Mega Pratice ID"),
         description=_(u"ID used to link multiple practices as one entity"),
         required=True,
+    )
+    fax_number = schema.TextLine(
+        title=_(u"Fax Number"),
+        required=False,
+    )
+    inbound_shipping_method = schema.Choice(
+        title=_(u"Inbound Shipping Method"),
+        description=_(u"Inbound Shipping Method"),
+        required="True",
+        values=[_(u"Fed-Ex"), _(u"UPS")],
+    )
+    kits_on_site = schema.Int(
+        title=_(u"Kits on Site"),
+        description=_(u"Kits on Site"),
+        required="True",
     )
