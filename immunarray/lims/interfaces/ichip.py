fb39309a0fb6ece331c000a09a5baf27fae5aa5b
Campbell <campbell@bikalabs.com>
2016-10-22 17:40:25 +0200

    Remove redundant ichip_ from ichip fields

--- immunarray/lims/interfaces/ichip.py
+++ immunarray/lims/interfaces/ichip.py
@@ -12,99 +12,99 @@ class IiChip(model.Schema):
     """An iChip Lot that will be the container class object
     """
 
-    ichip_lot_run_date = schema.Date(
+    lot_run_date = schema.Date(
         title=_(u"iChip Run Date"),
         description=_(u"Run Date of iChip"),
         required=False,
     )
 
-    ichip_status = schema.Choice(
+    status = schema.Choice(
         title=_(u"iChip Status"),
         description=_(u"Status of iChip"),
         required=True,
         values=[_(u'Quarantined'), _(u'Released'), _(u'Retained'), _(u'Inprocess'), _(u'Used-QC-Passed'), _(u'Used-QC-Failed'), _(u'Residual')],
     )
 
-    ichip_agilent_red = NamedBlobImage(
+    agilent_red = NamedBlobImage(
         title=_(u"iChip Agilent Red Image"),
         description=_(u"Agilent Red Image of iChip (.tiff)"),
         required=False,
     )
 
-    ichip_agilent_green = NamedBlobImage(
+    agilent_green = NamedBlobImage(
         title=_(u"iChip Agilent Green Image"),
         description=_(u"Agilent Green Image of iChip (.tiff)"),
         required=False,
     )
 
-    ichip_genepix_red = NamedBlobImage(
+    genepix_red = NamedBlobImage(
         title=_(u"iChip GenePix Red Feature Extraction"),
         description=_(u"GenePix Red Feature Extraction of iChip (.gpr)"),
         required=False,
     )
 
-    ichip_genepix_green = NamedBlobImage(
+    genepix_green = NamedBlobImage(
         title=_(u"iChip GenePix Green Feature Extraction"),
         description=_(u"GenePix Green Feature Extraction of iChip (.gpr)"),
         required=False,
     )
 
-    ichip_storage_location = schema.Choice(
+    storage_location = schema.Choice(
         title=_(u"iChip Storage Location"),
         description=_(u"Storage Location of iChip"),
         values=[_('EQ-76'), _('EQ-Unknown')],
         required=False,
     )
 
-    ichip_well_a = schema.TextLine(
+    well_a = schema.TextLine(
         title=_(u"iChip Well A"),
         description=_(u"Aliquot ID of Sample Placed in iChip Well A"),
         required=False,
     )
 
-    ichip_well_b = schema.TextLine(
+    well_b = schema.TextLine(
         title=_(u"iChip Well B"),
         description=_(u"Aliquot ID of Sample Placed in iChip Well B"),
         required=False,
     )
 
-    ichip_well_c = schema.TextLine(
+    well_c = schema.TextLine(
         title=_(u"iChip Well C"),
         description=_(u"Aliquot ID of Sample Placed in iChip Well C"),
         required=False,
     )
 
-    ichip_well_d = schema.TextLine(
+    well_d = schema.TextLine(
         title=_(u"iChip Well D"),
         description=_(u"Aliquot ID of Sample Placed in iChip Well D"),
         required=False,
     )
 
-    ichip_well_e = schema.TextLine(
+    well_e = schema.TextLine(
         title=_(u"iChip Well E"),
         description=_(u"Aliquot ID of Sample Placed in iChip Well E"),
         required=False,
     )
 
-    ichip_well_f = schema.TextLine(
+    well_f = schema.TextLine(
         title=_(u"iChip Well F"),
         description=_(u"Aliquot ID of Sample Placed in iChip Well F"),
         required=False,
     )
 
-    ichip_well_g = schema.TextLine(
+    well_g = schema.TextLine(
         title=_(u"iChip Well G"),
         description=_(u"Aliquot ID of Sample Placed in iChip Well G"),
         required=False,
     )
 
-    ichip_well_h = schema.TextLine(
+    well_h = schema.TextLine(
         title=_(u"iChip Well H"),
         description=_(u"Aliquot ID of Sample Placed in iChip Well H"),
         required=False,
     )
 
-    ichip_comment = RichText(
+    comment = RichText(
         title=_(u"iChip Comment"),
         description=_(u"Comments about iChip"),
         required=False,
