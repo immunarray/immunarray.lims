e659f63680da940d2784e01f6c9c6c8d5dca6b6a
Campbell <campbell@bikalabs.com>
2016-10-22 22:08:45 +0200

    Configure ichiplot creation subscriber to create ichips

--- immunarray/lims/subscribers/ichiplot.py
+++ immunarray/lims/subscribers/ichiplot.py
@@ -1,15 +1,42 @@
 # -*- coding: utf-8 -*-
-from immunarray.lims.permissions import AddiChip
 from bika.lims.permissions import disallow_default_contenttypes
+from immunarray.lims.permissions import AddiChip
+from plone.api.content import create
 
-def iChipLotAdded(instance, event):
-    """iChipLot has been added, some configuration and permissions
-    to set on the new folder
+
+def set_ichiplot_permissions(instance):
+    """Set default permissions for a new ichiplot
     """
     instance.manage_permission(
         AddiChip, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
     disallow_default_contenttypes(instance)
 
-    # I want the Title to be set from the ichiplot ID
+
+def set_ichiplot_title(instance):
+    """I want the Title to be set from the ichiplotID
+    """
     instance.title = instance.ichiplotID
     instance.reindexObject(idxs=['Title'])
+
+
+def create_ichips(instance):
+    """I want to create X amount of iChips from form value.
+    """
+
+    title = instance.ichiplotID
+    for x in range(1, instance.nr_ichips + 1):
+        ichip = create(container=instance,
+                       type='iChip',
+                       id="{0}_{1:02d}".format(title, x),
+                       title="{0}_{1:02d}".format(title, x))
+        # Configure each ichip to reflect the parent's settings
+        ichip.frames = instance.frames
+
+
+def iChipLotAdded(instance, event):
+    """iChipLot has been added, some configuration and permissions
+    to set on the new folder
+    """
+    set_ichiplot_permissions(instance)
+    set_ichiplot_title(instance)
+    create_ichips(instance)
