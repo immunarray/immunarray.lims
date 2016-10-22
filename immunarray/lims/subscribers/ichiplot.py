5a547143749a305909247067364856caa88f6377
Campbell <campbell@bikalabs.com>
2016-10-22 17:23:44 +0200

    When creating new IChipLots, set Title == IChipLotID

--- immunarray/lims/subscribers/ichiplot.py
+++ immunarray/lims/subscribers/ichiplot.py
@@ -3,8 +3,13 @@ from immunarray.lims.permissions import AddiChip
 from bika.lims.permissions import disallow_default_contenttypes
 
 def iChipLotAdded(instance, event):
-    """iChipLot has been added, some permissions to set on the new folder
+    """iChipLot has been added, some configuration and permissions
+    to set on the new folder
     """
     instance.manage_permission(
         AddiChip, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
     disallow_default_contenttypes(instance)
+
+    # I want the Title to be set from the ichiplot ID
+    instance.title = instance.ichiplotID
+    instance.reindexObject(idxs=['Title'])
