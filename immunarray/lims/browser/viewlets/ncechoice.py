32f23ff437d346ffe0d226e11cfa8ef85e8b28e4
Campbell <campbell@bikalabs.com>
2016-10-23 16:55:15 +0200

    remove dead code

--- immunarray/lims/browser/viewlets/ncechoice.py
+++ immunarray/lims/browser/viewlets/ncechoice.py
@@ -9,34 +9,3 @@ class NCEChoiceViewlet(ViewletBase):
 
     def render(self):
         return self.index()
-
-
-class AddMultipleiChipsHandler(BrowserView):
-
-    def __call__(self):
-        ichiplot = self.context
-
-        try:
-            start = int(self.request.form.get('sequence_start', ''))
-            end = int(self.request.form.get('sequence_end', ''))
-        except ValueError:
-            msg = u'Start and End must both be integers'
-            self.context.plone_utils.addPortalMessage(msg)
-            self.request.response.redirect(ichiplot.absolute_url())
-            return
-
-        if not start < end:
-            msg = u'End must be higher than Start.'
-            self.context.plone_utils.addPortalMessage(msg)
-            self.request.response.redirect(ichiplot.absolute_url())
-            return
-
-        title = ichiplot.ichiplotID
-
-        for x in range(start, end+1):
-            objectname = "{0}_{1}".format(title, x)
-            api.content.create(
-                type='iChip',
-                title=objectname,
-                container=ichiplot)
-        self.request.response.redirect(ichiplot.absolute_url())
