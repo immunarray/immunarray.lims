a65ca56105d241d8ccfcec5be6a8badd3e4150cd
immunarray <justin.pitts@immunarray.com>
2016-10-23 17:02:48 -0400

    vocab to be used for ichips in building runs

---
+++ immunarray/lims/vocabularies/ichip.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+from zope.component import queryUtility
+from zope.interface import implementer
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleVocabulary
+
+@implementer(IVocabularyFactory)
+class IChipsThatCanBeUsed (object):
+    pass
+    def __call__(self, context):
+        values = api.portal.get_registry_record('immunarray.category_primary')
+        normalizer = queryUtility(IIDNormalizer)
+        items = [(i, normalizer.normalize(i)) for i in values]
+        return SimpleVocabulary.fromItems(items)
+
+PrimaryNCEVocabulary = PrimaryNCE()
+
