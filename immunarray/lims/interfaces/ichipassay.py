263a7ac1c29a6a541db302449c7453b0e7296418
immunarray <justin.pitts@immunarray.com>
2017-01-11 15:29:40 -0500

    tweeks

--- immunarray/lims/interfaces/ichipassay.py
+++ immunarray/lims/interfaces/ichipassay.py
@@ -6,7 +6,6 @@ from immunarray.lims import messageFactory as _
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.dexterity.utils import createContentInContainer
 from plone.autoform import directives
-from z3c.form.browser.radio import RadioFieldWidget
 from zope.interface import alsoProvides
 
 class IiChipAssay(model.Schema):
@@ -18,13 +17,13 @@ class IiChipAssay(model.Schema):
         required=True,
     )
 
-    description = schema.Text(
+    description = schema.TextLine(
         title=_(u"Description"),
         description =_(u"Description"),
         required=False,
     )
 
-    ichiptype = schema.Choice(
+    ichiptype = schema.TextLine(
         title=_(u"iChip Type"),
         description =_(u"iChip Type"),
         required =True,
@@ -34,38 +33,33 @@ class IiChipAssay(model.Schema):
         title=_(u"Number of Unique iChips Needed"),
         description =_(u"Number of Unique iChips Needed"),
         required = True,
-        defalut = 1,
     )
 
     number_of_same_lot_replication_needed_for_samples = schema.Int(
         title=_(u"Same Lot Replication Needed For Samples"),
         description =_(u"Same Lot Replication Needed For Samples"),
         required = True,
-        defalut = 1,
     )
 
     number_of_high_value_controls = schema.Int(
         title=_(u"Number of High Value Controls"),
         description =_(u"Number of High Value Controls"),
         required = True,
-        defalut = 1,
     )
 
     number_of_low_value_controls = schema.Int(
         title=_(u"Number of Low Value Controls"),
         description =_(u"Number of Low Value Controls"),
         required = True,
-        defalut = 1,
     )
 
     sample_qc_dilution_factor = schema.Int(
         title=_(u"Number of High Value Controls"),
         description =_(u"Number of High Value Controls"),
         required = True,
-        defalut = 1,
     )
 
-    sample_qc_dilution_material = schema.(
+    sample_qc_dilution_material = schema.TextLine(
         title=_(u"Sample QC Diluation Material"),
         description =_(u"Sample QC Diluiton Material"),
         required = True,
@@ -75,27 +69,24 @@ class IiChipAssay(model.Schema):
         title=_(u"Max Number of Plates per Test Run"),
         description =_(u"Max Number of Plates per Test Run"),
         required = True,
-        defalut = 8,
     )
 
     number_of_working_aliquots_needed = schema.Int(
         title=_(u"Number of Working Aliquots Needed"),
         description =_(u"Number of Working Aliquots Needed"),
         required = True,
-        defalut = 1,
     )
 
     desired_working_aliquot_volume = schema.Int(
         title=_(u"Desired Working Aliquot Volume (uL)"),
         description =_(u"Desired Working Aliquot Volume (uL)"),
         required = True,
-        defalut = 12,
     )
 
-    comments = schema.Text(
+    comments = RichText(
         title = _(u"Comments"),
         description = _(u"Comments"),
         required = False,
     )
 
-alsoProvides( IFormFieldProvider)
+alsoProvides(IiChipAssay, IFormFieldProvider)
