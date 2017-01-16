c5a0b6646decb288cbfc1f804e8b2a949eab5a63
immunarray <justin.pitts@immunarray.com>
2017-01-16 13:55:11 -0500

    Clean up spacing and updated fields

--- immunarray/lims/interfaces/ichipassay.py
+++ immunarray/lims/interfaces/ichipassay.py
@@ -13,79 +13,87 @@ class IiChipAssay(model.Schema):
     """ Interface that will allow for creation of new iChip Assays"""
     name = schema.TextLine(
         title=_(u"Title"),
-        description =_(u"Title"),
+        description=_(u"Title"),
         required=True,
     )
 
     description = schema.TextLine(
         title=_(u"Description"),
-        description =_(u"Description"),
+        description=_(u"Description"),
         required=False,
     )
 
     ichiptype = schema.TextLine(
         title=_(u"iChip Type"),
-        description =_(u"iChip Type"),
-        required =True,
+        description=_(u"iChip Type"),
+        required=True,
     )
 
-    number_of_unique_ichips_needed = schema.Int(
-        title=_(u"Number of Unique iChips Needed"),
-        description =_(u"Number of Unique iChips Needed"),
-        required = True,
+    number_of_unique_ichips_lots_needed = schema.Int(
+        title=_(u"Number of Unique iChip Lots Needed"),
+        description=_(u"Number of Unique iChip Lots Needed"),
+        required=True,
+        default=2,
     )
 
     number_of_same_lot_replication_needed_for_samples = schema.Int(
         title=_(u"Same Lot Replication Needed For Samples"),
-        description =_(u"Same Lot Replication Needed For Samples"),
-        required = True,
+        description=_(u"Same Lot Replication Needed For Samples"),
+        required=True,
+        default=2,
     )
 
     number_of_high_value_controls = schema.Int(
-        title=_(u"Number of High Value Controls"),
-        description =_(u"Number of High Value Controls"),
-        required = True,
+        title=_(u"Number of High/Positive Controls"),
+        description=_(u"Number of High/Positive Controls"),
+        required=True,
+        default=2,
     )
 
     number_of_low_value_controls = schema.Int(
-        title=_(u"Number of Low Value Controls"),
-        description =_(u"Number of Low Value Controls"),
-        required = True,
+        title=_(u"Number of Low/Negative Controls"),
+        description=_(u"Number of Low/Negative Controls"),
+        required=True,
+        default=1,
     )
 
     sample_qc_dilution_factor = schema.Int(
-        title=_(u"Number of High Value Controls"),
-        description =_(u"Number of High Value Controls"),
-        required = True,
+        title=_(u"Material Dilution Factor"),
+        description=_(u"Material Dilution Factor"),
+        required=True,
+        default=80,
     )
 
     sample_qc_dilution_material = schema.TextLine(
         title=_(u"Sample QC Diluation Material"),
-        description =_(u"Sample QC Diluiton Material"),
-        required = True,
+        description=_(u"Sample QC Diluiton Material"),
+        required=True,
     )
 
     max_number_of_plates_per_test_run = schema.Int(
         title=_(u"Max Number of Plates per Test Run"),
-        description =_(u"Max Number of Plates per Test Run"),
-        required = True,
+        description=_(u"Max Number of Plates per Test Run"),
+        required=True,
+        default=8,
     )
 
     number_of_working_aliquots_needed = schema.Int(
         title=_(u"Number of Working Aliquots Needed"),
-        description =_(u"Number of Working Aliquots Needed"),
-        required = True,
+        description=_(u"Number of Working Aliquots Needed"),
+        required=True,
+        default=1,
     )
 
     desired_working_aliquot_volume = schema.Int(
         title=_(u"Desired Working Aliquot Volume (uL)"),
-        description =_(u"Desired Working Aliquot Volume (uL)"),
-        required = True,
+        description=_(u"Desired Working Aliquot Volume (uL)"),
+        required=True,
+        default=12,
     )
 
     comments = RichText(
         title = _(u"Comments"),
-        description = _(u"Comments"),
-        required = False,
+        description= _(u"Comments"),
+        required=False,
     )
 
