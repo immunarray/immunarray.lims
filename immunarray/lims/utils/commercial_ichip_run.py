728a20876cc6070829a776b0c98766ee7d1e9473
immunarray <justin.pitts@immunarray.com>
2017-01-30 11:54:32 -0500

    util for building commercial ichip runs

---
+++ immunarray/lims/utils/commercial_ichip_run.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+from plone.api.content import create
+from immunarray.lims.interfaces import ichip
+from immunarray.lims.interfaces import ichiplot
+from immunarray.lims.interfaces import commercial_eight_frame_run
+from immunarray.lims.interfaces import ichipassay
+
+#sample selection
+#aliquot selection
+
+#query to close iChipLots, and make changes based on inventory
+#reasons to close a lot
+#ichiplot.expiration_date is greater than current date
+#ichip count of any lot less than 1
+
+#set var iChipLot_Count
+
+#query iChipLots with "Passed" status, count results, set var Max_iChipLots
+
+#if loop for count >=10
+
+#than loop for count <10
+
+#query iChipLots that have "Passed" (want the oldest, based on ichiplot.print_date)
+
+#count iChips in "Released" state
+
+#if loop for count% by number of ichipassay.number_of_same_lot_replication_needed_for_samples
+
+    #save iChipLot and Chips to use (Lot, ichip.title)
+    #add 1 to iChipLot_Count
+
+#than loop, subtract 1 from count of iChips
+
+#set var plate_count=0
+
+def SelectiChips(event):
+    pass
+
