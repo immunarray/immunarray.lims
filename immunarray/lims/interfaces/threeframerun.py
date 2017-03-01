38cd9e4fb9521d3aae042fd57d45101228f0d52f
immunarray <justin.pitts@immunarray.com>
2017-03-01 16:00:03 -0500

    start point for dictionary of iChip+Well ID to aliquot tested

--- immunarray/lims/interfaces/threeframerun.py
+++ immunarray/lims/interfaces/threeframerun.py
@@ -1,14 +1,12 @@
 from datetime import date
-from immunarray.lims.interfaces import veracisrunbase
+from immunarray.lims.interfaces.veracisrunbase import IVeracisRunBase
 from immunarray.lims import messageFactory as _
 from plone.app.textfield import RichText
-from plone.namedfile.field import NamedBlobImage
 from plone.supermodel import model
 from zope import schema
 
 class IThreeFrameRun(IVeracisRunBase):
-    pass
-    status = schema.Dict(
-        key_type=schema.TextLine(title=u""),
-        values_type=schema.TextLine(title=u"iChip_ID")
+    aliquot_to_well = schema.Dict(
+        key_type=schema.TextLine(title=u"Aliquot ID"),
+        value_type=schema.TextLine(title=u"iChip and Well ID V6.1_33-1")
     )
