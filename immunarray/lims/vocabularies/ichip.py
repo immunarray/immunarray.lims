a2677eafaa60f890004e46cce5acbe49a114905c
immunarray <justin.pitts@immunarray.com>
2017-03-01 15:58:57 -0500

    created vocabs to sort all iChips in the US and all iChips for commercial testing

--- immunarray/lims/vocabularies/ichip.py
+++ immunarray/lims/vocabularies/ichip.py
@@ -7,13 +7,47 @@ from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
 
 @implementer(IVocabularyFactory)
-class IChipsThatCanBeUsed (object):
-    pass
+class IChipsInUS (object):
     def __call__(self, context):
-        values = api.portal.get_registry_record('immunarray.category_primary')
+        values = context.ichip.objectValues()
+        ichips = [v.title for v in values
+                  if 'released' in v.status.lower()
+                  or 'retained-us' in v.status.lower()
+                  or 'quarantined' in v.status.lower()
+                  or 'retained-us' in v.status.lower()]
         normalizer = queryUtility(IIDNormalizer)
-        items = [(i, normalizer.normalize(i)) for i in values]
+        items = [(i, normalizer.normalize(i)) for i in ichips]
+IChipsInUSVocabulary = IChipsInUS()
+
+class IChipsForCommercialTesting (object):
+    def __call__(self, context):
+        values = context.ichip.objectValues()
+        ichips = [v.title for v in values
+                  if 'released' in v.status.lower()]
+        normalizer = queryUtility(IIDNormalizer)
+        items = [(i, normalizer.normalize(i)) for i in ichips]
+IChipsForCommercialTestingVocabulary = IChipsForCommercialTesting()
+
+"""class Providers(object):
+    def __call__(self, context):
+        values = context.providers.objectValues()
+        names = [" ".join([str(v.site_ID), v.first_name, v.last_name]) for v in values]
+        normalizer = queryUtility(IIDNormalizer)
+        items = [(n, normalizer.normalize(n)) for n in names]
         return SimpleVocabulary.fromItems(items)
 
-PrimaryNCEVocabulary = PrimaryNCE()
+ProvidersVocabulary = Providers()
 
+values=[_(u'Quarantined'),
+                _(u'Released'),
+                _(u'Retained-US'),
+                _(u'Retained-IA'),
+                _(u'Inprocess'),
+                _(u'Used-QC-Passed'),
+                _(u'Used-QC-Failed'),
+                _(u'Residual'),
+                _(u'Broken'),
+                _(u'Used-Training'),
+                _(u'Used-Validaiton')],
+    )
+    """
