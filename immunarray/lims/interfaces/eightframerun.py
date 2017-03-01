41e33511ef57626df384b1c5d92f9fbf801abe53
immunarray <justin.pitts@immunarray.com>
2017-03-01 16:02:47 -0500

    working on adding a button to drive choices on what the test run will be.  (click button for commercial run, will limit the iChips that can be pulled ect)

--- immunarray/lims/interfaces/eightframerun.py
+++ immunarray/lims/interfaces/eightframerun.py
@@ -1,5 +1,20 @@
 from datetime import date
 from immunarray.lims.interfaces.veracisrunbase import IVeracisRunBase
+from z3c.form import button
+from z3c.form.form import EditForm
+"""Add buttons to a form don't think it goes in the interface!"""
+class Form(EditForm):
+    @button.buttonAndHandler(
+        u"Commercail Run",
+        name="CommercalRun",
+        condition=lambda form: form.okToDelete()
+    )
+    def handleDelete(self, action):
+        """
+            Delete this event.
+        """
+        pass
+        self.status = "Event deleted."
 
 class IEightFrameRun(IVeracisRunBase):
     pass
