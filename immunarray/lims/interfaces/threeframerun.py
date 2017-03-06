5b1e4a0f7eacea981856e906bb7205e1acfd711a
immunarray <justin.pitts@immunarray.com>
2017-03-06 16:14:57 -0500

    working on vocab within a dict (broken)

--- immunarray/lims/interfaces/threeframerun.py
+++ immunarray/lims/interfaces/threeframerun.py
@@ -4,9 +4,16 @@ from immunarray.lims import messageFactory as _
 from plone.app.textfield import RichText
 from plone.supermodel import model
 from zope import schema
+from immunarray.lims.vocabularies.ichip import IChipsInUSVocabulary
+from immunarray.lims.vocabularies.ichip import IChipsForCommercialTestingVocabulary
+from immunarray.lims.vocabularies import ichip
+from immunarray.lims.vocabularies.ichipassay import IChipAssayListVocabulary
+from immunarray.lims.vocabularies import ichipassay
+from immunarray.lims.vocabularies.provider import ProvidersVocabulary
+
 
 class IThreeFrameRun(IVeracisRunBase):
     aliquot_to_well = schema.Dict(
         key_type=schema.TextLine(title=u"Aliquot ID"),
-        value_type=schema.TextLine(title=u"iChip and Well ID V6.1_33-1")
+        value_type=schema.Choice(vocabulary='immunarray.lims.vocabularies.ichip.IChipsInUSVocabulary')
     )
