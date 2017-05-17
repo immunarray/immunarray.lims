c0fba6eb21a3884ad58871d70012eb33a11dd4db
immunarray <justin.pitts@immunarray.com>
2017-05-17 11:39:39 -0400

    still not working?

--- immunarray/lims/static/js/rec.js
+++ immunarray/lims/static/js/rec.js
@@ -1,26 +1,10 @@
 require([
   'jquery'
-], function($){
-
-  var cycle = function(){
-    $('h1').animate({
-      left: '250px',
-      opacity: '0.5',
-      'font-size': '30px'
-    }, function(){
-      $('h1').animate({
-        left: '0',
-        opacity: '1',
-        'font-size': '20px'
-      }, function(){
-        setTimeout(function(){
-          cycle();
-        }, 2000);
-      });
-    });
-  };
-
-  $(document).ready(function(){
-    cycle();
-  });
+],
+  (function($) {
+    $(function() {
+      var $usn=$('.usn')
+      alert($usn);
 });
+});
+);
