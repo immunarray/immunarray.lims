d8140082f7a7b7b8479be940c39206b001d4e627
immunarray <justin.pitts@immunarray.com>
2017-05-17 16:53:12 -0400

    working jQuery

--- immunarray/lims/static/js/rec.js
+++ immunarray/lims/static/js/rec.js
@@ -4,7 +4,25 @@ require([
   (function($) {
     $(function() {
       var $usn=$('.usn')
-      alert($usn);
-});
-});
-);
+      var $ptFirstName =$('.patient_first_name')
+      var $ptLastName =$('.patient_last_name')
+      var $ptdob = $('.dob')
+
+      $('#usn').on("change", function(){
+        alert("USN was changed")
+      });
+
+      $('#patient_first_name').on("change", function(){
+        alert("PT first name was changed")
+      });
+
+      $('#patient_last_name').on("change", function(){
+         alert("PT last name was changed")
+      });
+
+      $('#dob').on("change", function(){
+         alert("PT DOB was changed")
+      });
+})
+})
+)
