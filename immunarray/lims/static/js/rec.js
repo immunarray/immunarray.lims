aca47d33ee501c071f593ab35d5f8a79e13771e8
immunarray <justin.pitts@immunarray.com>
2017-05-18 13:45:20 -0400

    in progress, parse usn to make site and usn seperate from data input.

--- immunarray/lims/static/js/rec.js
+++ immunarray/lims/static/js/rec.js
@@ -3,13 +3,17 @@ require([
 ],
   (function($) {
     $(function() {
-      var $usn=$('.usn')
+      var $usn=$('#usn')
+
       var $ptFirstName =$('.patient_first_name')
       var $ptLastName =$('.patient_last_name')
       var $ptdob = $('.dob')
 
       $('#usn').on("change", function(){
         //alert("USN was changed")
+        var $usn=$('#usn')
+        var usnParts = $(usn).val().split("-");
+        return usnParts;
       });
 
       $('#patient_first_name').on("change", function(){
@@ -22,6 +26,37 @@ require([
 
       $('#dob').on("change", function(){
          //alert("PT DOB was changed")
+      });
+
+      // Make form easier to use, hide all boxes that have click dependency
+      $('#ethnicity_specify').hide();
+      $('#ethnicity_other').on('click', function(){
+        $(this).next().slideToggle(400);
+      });
+
+      $('#test-other-specify').hide();
+      $('#test-other').on('click', function(){
+        $(this).next().slideToggle(400);
+      });
+
+      $('#clin-joint-pain-specify').hide();
+      $('#clin-joint-pain').on('click', function(){
+        $(this).next().slideToggle(400);
+      });
+
+      $('#clin-inflam-specify').hide();
+      $('#clin-inflam').on('click', function(){
+              $(this).next().slideToggle(400);
+            });
+
+      $('#clin-other-specify').hide();
+      $('#clin-other').on('click', function(){
+        $(this).next().slideToggle(400);
+      });
+
+      $('#diag-other-specify').hide();
+      $('#diag-other').on('click', function(){
+        $(this).next().slideToggle(400);
       });
 })
 })
