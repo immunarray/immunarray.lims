986fa8ccc8d4e51e8c4b3724dd4e3c70daa214a3
immunarray <justin.pitts@immunarray.com>
2017-06-01 09:05:49 -0400

    make patient python file

---
+++ immunarray/lims/subscribers/patient.py
@@ -0,0 +1,21 @@
+from immunarray.lims.permissions import AddPatient
+from plone import api
+from bika.lims.permissions import disallow_default_contenttypes
+
+def MakePatient(self, pt, dob, marital_status, gender,
+                          ssn, mrn, consent_acquired, ethnicity):
+    #set permission for new patient record
+    pt.manage_permission(
+        AddPatient, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
+    disallow_default_contenttypes(pt)
+
+    patient = api.content.create(container=pt,
+                                 type = 'Patient',
+                                 dob = dob,
+                                 marital_status = marital_status,
+                                 gender = gender,
+                                 ssn = ssn,
+                                 medical_record_number = mrn,
+                                 research_consent = consent_acquired,
+                                 race = ethnicity,
+                                 )
