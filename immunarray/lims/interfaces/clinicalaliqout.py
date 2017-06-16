b07f3415e5c2738d6e38814d121afaceba3de117
immunarray <justin.pitts@immunarray.com>
2017-06-16 10:50:23 -0400

    rebased Clinical Aliquot to be model.Scheam not IAliqout

--- immunarray/lims/interfaces/clinicalaliqout.py
+++ immunarray/lims/interfaces/clinicalaliqout.py
@@ -1,11 +1,25 @@
 from bika.lims.interfaces.aliquot import IAliquot
 from datetime import datetime
+from immunarray.lims import messageFactory as _
 from plone.supermodel import model
 from zope import schema
 from immunarray.lims.vocabularies.ichipassay import IChipAssayListVocabulary
 
-class IClinicalAliquot(IAliquot):
+class IClinicalAliquot(model.Schema):
+
+    title = schema.TextLine(
+        title=_(u"Aliquot Name"),
+        description=_(u"Aliquot Name/ID"),
+        required=False,
+    )
+
     result = schema.Dict(
         key_type=schema.Choice(source=IChipAssayListVocabulary, required=False),
         value_type=schema.TextLine,
+    )
+
+    sample_id = schema.TextLine(
+        title=_(u"Parent ID"),
+        description=_(u"Parent ID"),
+        required=False,
     )
