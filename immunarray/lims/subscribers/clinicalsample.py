85ab41ad7e62dcd24b0e94822bd207365192b661
immunarray <justin.pitts@immunarray.com>
2017-06-19 11:43:33 -0400

    update spacing

--- immunarray/lims/subscribers/clinicalsample.py
+++ immunarray/lims/subscribers/clinicalsample.py
@@ -3,9 +3,8 @@ from immunarray.lims.permissions import AddClinicalAliquot
 from bika.lims.permissions import disallow_default_contenttypes
 
 
-def Added(instance, event):
-    """A new Rack has been created!
+def ClinicalSampleAdded(instance, event):
+    """A new Clinical Sample has been created!
     """
     instance.manage_permission(AddClinicalAliquot, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
-
     disallow_default_contenttypes(instance)
