b02b9413a5a4ce5b704412ed52a6bafe3066f69d
immunarray <justin.pitts@immunarray.com>
2017-06-26 10:58:14 -0400

    subscriber for clinical sample being added, allow for clinical aliquots to be added to the new object

--- immunarray/lims/subscribers/clinicalsample.py
+++ immunarray/lims/subscribers/clinicalsample.py
@@ -1,10 +1,27 @@
 # -*- coding: utf-8 -*-
 from immunarray.lims.permissions import AddClinicalAliquot
+from immunarray.lims.permissions import AddClinicalSample
 from bika.lims.permissions import disallow_default_contenttypes
-
+from bika.lims.utils.limsroot import getLims
 
 def ClinicalSampleAdded(instance, event):
     """A new Clinical Sample has been created!
     """
-    instance.manage_permission(AddClinicalAliquot, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
+    mp = instance.manage_permission
+    mp(AddClinicalAliquot, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
+    # Don't allow samples to be nested in each other!
+    # mp(AddClinicalSample, [], 0)
     disallow_default_contenttypes(instance)
+
+
+def RandDSampleAdded(instance, event):
+    mp = instance.manage_permission
+    disallow_default_contenttypes(instance)
+    pass
+
+
+def QCSampleAdded(instance, event):
+    mp = instance.manage_permission
+    disallow_default_contenttypes(instance)
+    pass
+
