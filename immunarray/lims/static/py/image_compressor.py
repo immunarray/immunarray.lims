82209c16a6a0b6b4f98af076b6680de41832b145
immunarray <justin.pitts@immunarray.com>
2017-06-29 10:34:36 -0400

    initial commit for image compresser to be used on large images that do not need to be viewed only stored in the LIMS.

---
+++ immunarray/lims/static/py/image_compressor.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+pass
+import glob
+import os,sys,gzip
+
+"""filenames = glob.glob("/home/jpitts/Desktop/Images/*.tif")"""
+folder = './'
+for filename in os.listdir(folder):
+    infilename = os.path.join(folder,filename)
+    if not os.path.isfile(infilename): 
+        continue
+    oldbase = os.path.splitext(filename)
+    newname = infilename.replace('.tif', '.gz')
+    print newname
+    
+"""f_out = gzip.open(new_name,'wb')
+f_out.close()
+f_in.close()
+
+f_out = gzip.open('IA_R38.21_1_S001_Green.gz', 'wb')
+f_out.writelines(f_in)
+f_out.close()
+f_in.close()
+
+
+filenames = glob.glob("/home/jpitts/Desktop/Images/*.tif")
+for f in filenames:
+    f_in = open(f, 'rb')
+    f_out = gzip.open(f, 'wb')
+    f_out.writelines(f_in)
+    f_out.close()
+    f_in.close()
+"""
+    
+"""gzip.open(f, 'wb') as g;
+        g.write()"""
+
+
