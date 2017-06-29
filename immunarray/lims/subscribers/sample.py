cf840734ca881d9bdad32fa91577f1f57db6af88
immunarray <justin.pitts@immunarray.com>
2017-06-29 10:26:54 -0400

    remove addQC and addRandD samples from child object

--- immunarray/lims/subscribers/sample.py
+++ immunarray/lims/subscribers/sample.py
@@ -8,10 +8,11 @@ def ClinicalSampleAdded(clinicalsample, event):
     """A new Clinical Sample has been created!
     """
     clinicalsample.manage_permission(AddClinicalAliquot, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
-    # clinicalsample.manage_permission(AddClinicalSample, [], 0)
-    import pdb;pdb.set_trace()
     # Don't allow samples to be nested in each other!
-    # disallow_default_contenttypes(clinicalsample)
+    clinicalsample.manage_permission(AddClinicalSample, [], 0)
+    clinicalsample.manage_permission(AddRandDAliquot, [], 0)
+    clinicalsample.manage_permission(AddQCAliquot, [], 0)
+    disallow_default_contenttypes(clinicalsample)
 
 
 def RandDSampleAdded(randdsample, event):
