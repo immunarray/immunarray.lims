9df102df6c91e0cdbd6f34af62eb01c8368cec60
immunarray <justin.pitts@immunarray.com>
2017-06-29 16:18:42 -0400

    update fields to hold needed data

--- immunarray/lims/interfaces/clinicalaliquot.py
+++ immunarray/lims/interfaces/clinicalaliquot.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from bika.lims.interfaces.aliquot import IAliquot
-from datetime import datetime
+from datetime import datetime, date
 from immunarray.lims import messageFactory as _
 from plone.supermodel import model
 from zope import schema
@@ -15,6 +15,39 @@ class IClinicalAliquot(model.Schema):
         required=False,
     )
 
+    sample_id = schema.TextLine(
+        title=_(u"Parent ID"),
+        description=_(u"Parent ID"),
+        required=False,
+    )
+
+    aliquot_type = schema.Choice(
+        title=_(u"Aliquot Type"),
+        description=_(u"Aliquot Type"),
+        required=True,
+        values=[_(u'Bulk'),
+                _(u'Working')],
+    )
+
+    volume = schema.Int(
+        title = _(u"Volume (in uL)"),
+        description = _(u"Volume (in uL)"),
+        required = False,
+    )
+
+    pour_date = schema.Date(
+        title=_(u"Aliquot Pour Date"),
+        description =_(u"Aliquot Pour Date"),
+        required=False,
+        default=date.today(),
+    )
+
+    consume_date = schema.Date(
+        title=_(u"Aliquot Consume Date"),
+        description =_(u"Aliquot Consume Date"),
+        required=False,
+    )
+
     result = schema.Dict(
         key_type=schema.Choice(source=IChipAssayListVocabulary, required=False),
         value_type=schema.TextLine(
@@ -22,12 +55,6 @@ class IClinicalAliquot(model.Schema):
             description=_(u"Result Value"),
             required=False,
         )
-    )
-
-    sample_id = schema.TextLine(
-        title=_(u"Parent ID"),
-        description=_(u"Parent ID"),
-        required=False,
     )
 
     images = schema.Dict(
