ed6545cb2d50fddef5280d268dff33474f5ce3e3
immunarray <justin.pitts@immunarray.com>
2017-06-30 15:54:56 -0400

    non working browser view for commercial test run

--- immunarray/lims/browser/analysisrequest/commercial_run.py
+++ immunarray/lims/browser/analysisrequest/commercial_run.py
@@ -26,7 +26,50 @@ class AddCommercialEightFrameTestRunView(BrowserView):
         self.request = request
         self.errors = []
 
+
     def __call__(self):
         add_resource_on_request(self.request, "static.js.commercial_run.js")
-        request = self.request
+        # request = self.request
+        #assay = self.request('testtorun')
+
+        #if "buildrun" in request.form:
+        #    authenticator = request.form.get('_authenticator')
+        #    try:
+
         return self.template()
+
+
+    def querysamples(self, assay):
+        """Get all the samples that have pending test and arrange them by
+        collection date
+        """
+
+        values = api.content.find(context=api.portal.get(), portal_type='ClinicalSample')
+        sample_ids_to_be_tested = []
+        # get ClinicalSamples where values.sample_status = 'Received'
+        for u in values:
+            if u.sample_status == 'Received':
+                uids = [u.UID for u in values]
+        # get UIDs
+        pass
+
+
+
+    def getiChipsForTesting(self, assay, sample_count):
+        """Get iChips needed for testing
+        """
+        pass
+        # Get iChipLots that are not expired
+        # ichip_lot_expiration_date
+        # Get iChipLots that have the correct layout
+        # frames == '8 Frame iChips'
+        # Get iChipLots that are for the correct test
+        # intended_assay = 'SLEKEY-RO-V2-0-COMMERCIAL'
+        # Get iChipLots that passed QC process
+        # iChipAcceptanceStatus =='Passed'
+        # Have to open up the iChipLots, then walk them!
+        values = api.content.find(context=api.portal.get(), portal_type='iChipLot')
+        ichip_uids = [u.UID for u in values]
+        for i in ichip_uids:
+
+
