2a0cb5340633ffdcb57f50d119e23876d3f5db9a
immunarray <justin.pitts@immunarray.com>
2017-07-11 11:52:57 -0400

    semi working js file for test run pattern

--- immunarray/lims/static/js/test_run.js
+++ immunarray/lims/static/js/test_run.js
@@ -1,7 +1,7 @@
-/**
- * Brain for SLE-key_v_2_0_commercial.pt=
-*require([
-*  'jquery'
-*],
-* (function($) {})
-*/
+//Brain for SLE-key_v_2_0_commercial.pt
+require(['jquery'],
+
+(function($) {
+    var testChoice = document.querySelector('#assay_selection').value;
+    document.getElementById("buildselectedrun").onclick = alert(testChoice);
+    }))
