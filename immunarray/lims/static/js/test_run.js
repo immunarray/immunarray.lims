3e73715232ed47cde9440c4e68effa5b37900676
immunarray <justin.pitts@immunarray.com>
2017-07-13 09:15:46 -0400

    ajax send assay selection from the drop down to server side

--- immunarray/lims/static/js/test_run.js
+++ immunarray/lims/static/js/test_run.js
@@ -4,6 +4,22 @@ require(['jquery'],
 (function($) {
     $('#assay_selection').change(function() {
         var assaySelected = $(this).val()
+        authenticator = $('input[name="_authenticator"]').val();
         alert (assaySelected)
+        $.ajax({
+            url: 'ctest',
+            type: 'POST',
+            data: {
+                'assaySeleced': assaySelected,
+                '_authenticator': authenticator},
+            success: function(responseText, statusText, statusCode, xhr, $form){
+            if (statusCode.status === 210){
+                alert("No Samples Require this Assay Choice!!!")
+            }
+            //var testPlanData = JSON.parse(responseText)
+            //break the reply and fill the form.
+            //will need to change inputs to choice fields with aliquot ids
+            }
+        });
     })
 }))
