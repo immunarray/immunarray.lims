c9791bed1874a39a48a014891429ff5fcf21d738
immunarray <justin.pitts@immunarray.com>
2017-07-18 14:09:03 -0400

    added fields from base class as a comment for ease of development

--- immunarray/lims/interfaces/eightframerun.py
+++ immunarray/lims/interfaces/eightframerun.py
@@ -18,4 +18,51 @@ class Form(EditForm):
         self.status = "Event deleted."
 
 class IEightFrameRun(IVeracisRunBase):
+    """Eight well iChip test run
+    """
+    # aliquots go on iChip
+    # iChips go into test run
+    # test form is how the data will be interacted with!
     pass
+
+"""
+fields from base object
+class IVeracisRunBase(model.Schema):
+    """Object that will be the base of all veracis runs
+"""
+veracis_run_number = schema.Int(
+    title=_(u"Veracis Run Number"),
+    description=_(u"Veracis Run Number"),
+    required=True,
+)
+veracis_run_purpose = schema.TextLine(
+    title=_(u"Veracis Test Run Purpose"),
+    description=_(u"Veracis Test Run Purpose"),
+)
+veracis_run_serial_number = schema.Int(
+    title=_(u"Veracis Run Serial Number"),
+    description=_(u"Veracis Run Serial Number"),
+)
+veracis_run_operator = schema.TextLine(
+    title=_(u"Veracis Run Operator"),
+    description=_(u"Veracis Run Operator"),
+    required=True,
+)
+veracis_test_run_date = schema.Date(
+    title=_(u"Veracis Test Run Date"),
+    description=_(u"Veracis Test Run Date (MM/DD/YYYY)"),
+    default=date.today(),
+)
+veracis_test_scan_date = schema.Date(
+    title=_(u"Veracis Test Scan Date"),
+    description=_(u"Veracis Test Scan Date (MM/DD/YYYYY)"),
+    default=date.today(),
+)
+pdf_veracis_run = NamedBlobImage(
+    title=_(u"PDF Upload of Test Form"),
+    description=_(u"PDF Upload of Test Form"),
+    required=False,
+)
+
+alsoProvides(IVeracisRunBase, IFormFieldProvider)
+"""
