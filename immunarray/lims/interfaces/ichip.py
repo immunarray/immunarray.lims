3c45347ef29054e79a23ff031a743da45e1ce51e
immunarray <justin.pitts@immunarray.com>
2017-08-22 11:46:48 -0400

    changing well, image, extraction to be dictionary fields for growth options, the input will be controlled by the interfaces that will build and populate the objects

--- immunarray/lims/interfaces/ichip.py
+++ immunarray/lims/interfaces/ichip.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from immunarray.lims import messageFactory as _
 from plone.app.textfield import RichText
-from plone.namedfile.field import NamedBlobImage
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 from plone.supermodel import model
 from zope import schema
 
@@ -30,42 +30,43 @@ class IiChip(model.Schema):
                 _(u'Released'),
                 _(u'Retained-US'),
                 _(u'Retained-IA'),
-                _(u'Inprocess'),
+                _(u'In-Process'),
                 _(u'Used-QC-Passed'),
                 _(u'Used-QC-Failed'),
                 _(u'Residual'),
                 _(u'Broken'),
                 _(u'Used-Training'),
-                _(u'Used-Validaiton')],
+                _(u'Used-Validation')],
         default=_(u'Quarantined'),
     )
 
-    agilent_red = NamedBlobImage(
-        title=_(u"iChip Agilent Red Image"),
-        description=_(u"Agilent Red Image of iChip (.tiff)"),
-        required=False,
+    # Image name to actual image (zip image file before upload)
+    image = schema.Dict(
+        key_value=schema.TextLine(
+            title=_(u"Image Title (ie Agilent Red)"),
+            descrition=_(u"Image Title (ie Agilent Red)"),
+            required=False,
+        ),
+        value_type=NamedBlobImage(
+            title=_(u"iChip Agilent Image"),
+            description=_(u"Agilent Image of iChip (.tiff)"),
+            required=False),
     )
 
-    agilent_green = NamedBlobImage(
-        title=_(u"iChip Agilent Green Image"),
-        description=_(u"Agilent Green Image of iChip (.tiff)"),
-        required=False,
-    )
-
-    genepix_red = NamedBlobImage(
-        title=_(u"iChip GenePix Red Feature Extraction"),
-        description=_(u"GenePix Red Feature Extraction of iChip (.gpr)"),
-        required=False,
-    )
-
-    genepix_green = NamedBlobImage(
-        title=_(u"iChip GenePix Green Feature Extraction"),
-        description=_(u"GenePix Green Feature Extraction of iChip (.gpr)"),
-        required=False,
+    # Image extraction name to extraction
+    image_extractions = schema.Dict(
+        key_value=schema.TextLine(
+            title=_(u"Image Extraction Title (ie Agilent Red)"),
+            descrition=_(u"Image Extraction Title (ie Agilent Red)"),
+            required=False,
+        ),
+        value_type=NamedBlobFile(
+            title=_(u"iChip Agilent Image Extraction"),
+            description=_(u"Agilent Image Extraction of iChip (.tiff)"),
+            required=False),
     )
 
     # update to have vocabulary tied to choices
-
     storage_location = schema.Choice(
         title=_(u"iChip Storage Location"),
         description=_(u"Storage Location of iChip"),
@@ -73,52 +74,18 @@ class IiChip(model.Schema):
         required=False,
     )
 
-    well_a = schema.TextLine(
-        title=_(u"iChip Well A"),
-        description=_(u"Aliquot ID of Sample Placed in iChip Well A"),
-        required=False,
-    )
-
-    well_b = schema.TextLine(
-        title=_(u"iChip Well B"),
-        description=_(u"Aliquot ID of Sample Placed in iChip Well B"),
-        required=False,
-    )
-
-    well_c = schema.TextLine(
-        title=_(u"iChip Well C"),
-        description=_(u"Aliquot ID of Sample Placed in iChip Well C"),
-        required=False,
-    )
-
-    well_d = schema.TextLine(
-        title=_(u"iChip Well D"),
-        description=_(u"Aliquot ID of Sample Placed in iChip Well D"),
-        required=False,
-    )
-
-    well_e = schema.TextLine(
-        title=_(u"iChip Well E"),
-        description=_(u"Aliquot ID of Sample Placed in iChip Well E"),
-        required=False,
-    )
-
-    well_f = schema.TextLine(
-        title=_(u"iChip Well F"),
-        description=_(u"Aliquot ID of Sample Placed in iChip Well F"),
-        required=False,
-    )
-
-    well_g = schema.TextLine(
-        title=_(u"iChip Well G"),
-        description=_(u"Aliquot ID of Sample Placed in iChip Well G"),
-        required=False,
-    )
-
-    well_h = schema.TextLine(
-        title=_(u"iChip Well H"),
-        description=_(u"Aliquot ID of Sample Placed in iChip Well H"),
-        required=False,
+    # well to aliquot ID tested on it
+    well_to_aliquot = schema.Dict(
+        key_value=schema.TextLine(
+            title=_(u"Well ID"),
+            descrition=_(u"Well ID"),
+            required=False,
+        ),
+        value_type=schema.TextLine(
+            title=_(u"Aliquot ID"),
+            descrition=_(u"Aliquot ID"),
+            required=False,
+        ),
     )
 
     comment = RichText(
@@ -126,4 +93,3 @@ class IiChip(model.Schema):
         description=_(u"Comments about iChip"),
         required=False,
     )
-
