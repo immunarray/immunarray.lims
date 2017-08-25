342e4b76241736835dbaee2073be0e3924320627
immunarray <justin.pitts@immunarray.com>
2017-08-25 15:04:35 -0400

    update to add title from other input

--- immunarray/lims/interfaces/site.py
+++ immunarray/lims/interfaces/site.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from zope import schema
-from zope.interface import Interface
-from immunarray.lims import messageFactory as _
+
+from Products.CMFPlone.utils import safe_hasattr
 from bika.lims.interfaces.organisation import IOrganisation
+from immunarray.lims import messageFactory as _
+from plone.app.content.interfaces import INameFromTitle
+from plone.rfc822.interfaces import IPrimaryFieldInfo
+from zope.component import adapter
+from zope.interface import Interface
+from zope.interface import implementer
 
 
 class ISite(IOrganisation):
@@ -96,3 +102,28 @@ class ISite(IOrganisation):
         description=_(u"Site Notes"),
         required=False,
     )
+
+
+class INameFromFileName(Interface):
+    """Marker interface to enable name from filename behavior"""
+
+
+@implementer(INameFromTitle)
+@adapter(INameFromFileName)
+class NameFromFileName(object):
+
+    def __new__(cls, context):
+        info = IPrimaryFieldInfo(context, None)
+        if info is None:
+            return None
+        filename = getattr(info.value, 'filename', None)
+        if not isinstance(filename, basestring) or not filename:
+            return None
+        instance = super(NameFromFileName, cls).__new__(cls)
+        instance.title = filename
+        if safe_hasattr(context, 'title') and not context.title:
+            context.title = filename
+        return instance
+
+    def __init__(self, context):
+        pass
