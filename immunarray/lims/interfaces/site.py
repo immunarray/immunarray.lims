37cd8bd5281edf2e5b9eb7702ce35be3e098f581
immunarray <justin.pitts@immunarray.com>
2017-08-25 19:05:24 -0400

    no changes made

--- immunarray/lims/interfaces/site.py
+++ immunarray/lims/interfaces/site.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
 from zope import schema
-
 from Products.CMFPlone.utils import safe_hasattr
 from bika.lims.interfaces.organisation import IOrganisation
 from immunarray.lims import messageFactory as _
-from plone.app.content.interfaces import INameFromTitle
-from plone.rfc822.interfaces import IPrimaryFieldInfo
-from zope.component import adapter
-from zope.interface import Interface
-from zope.interface import implementer
 
 
 class ISite(IOrganisation):
@@ -104,26 +98,3 @@ class ISite(IOrganisation):
     )
 
 
-class INameFromFileName(Interface):
-    """Marker interface to enable name from filename behavior"""
-
-
-@implementer(INameFromTitle)
-@adapter(INameFromFileName)
-class NameFromFileName(object):
-
-    def __new__(cls, context):
-        info = IPrimaryFieldInfo(context, None)
-        if info is None:
-            return None
-        filename = getattr(info.value, 'filename', None)
-        if not isinstance(filename, basestring) or not filename:
-            return None
-        instance = super(NameFromFileName, cls).__new__(cls)
-        instance.title = filename
-        if safe_hasattr(context, 'title') and not context.title:
-            context.title = filename
-        return instance
-
-    def __init__(self, context):
-        pass
