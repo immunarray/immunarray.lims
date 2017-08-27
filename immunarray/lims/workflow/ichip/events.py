44134c05ad32ff4119ef8b563a29e1450ec7948d
Campbell <campbell@bikalabs.com>
2017-08-27 13:53:07 +0200

    PEP8 of ichip events and guards stub files

--- immunarray/lims/workflow/ichip/events.py
+++ immunarray/lims/workflow/ichip/events.py
@@ -3,90 +3,108 @@ def after_broken(self, instance):
     """
     pass
 
+
 def after_discard(self, instance):
     """
     """
     pass
+
 
 def after_expire(self, instance):
     """
     """
     pass
 
+
 def after_expired(self, instance):
     """
     """
     pass
+
 
 def after_in_process(self, instance):
     """
     """
     pass
 
+
 def after_queue(self, instance):
     """
     """
     pass
+
 
 def after_reject(self, instance):
     """
     """
     pass
 
+
 def after_release(self, instance):
     """
     """
     pass
+
 
 def after_released(self, instance):
     """
     """
     pass
 
+
 def after_reserve_for_stability_testing(self, instance):
     """
     """
     pass
+
 
 def after_residual(self, instance):
     """
     """
     pass
 
+
 def after_retain_ial(self, instance):
     """
     """
     pass
+
 
 def after_retain_usa(self, instance):
     """
     """
     pass
 
+
 def after_use_for_release_testing(self, instance):
     """
     """
     pass
+
 
 def after_use_for_stability_testing(self, instance):
     """
     """
     pass
 
+
 def after_use_for_training(self, instance):
     """
     """
     pass
+
 
 def after_use_for_validation(self, instance):
     """
     """
     pass
 
+
 def after_used_qc_fail(self, instance):
     """
     """
     pass
+
 
 def after_used_qc_pass(self, instance):
     """
--- immunarray/lims/workflow/ichip/guards.py
+++ immunarray/lims/workflow/ichip/guards.py
@@ -3,90 +3,108 @@ def guard_broken(instance):
     """
     return True
 
+
 def guard_discard(instance):
     """
     """
     return True
+
 
 def guard_expire(instance):
     """
     """
     return True
 
+
 def guard_expired(instance):
     """
     """
     return True
+
 
 def guard_in_process(instance):
     """
     """
     return True
 
+
 def guard_queue(instance):
     """
     """
     return True
+
 
 def guard_reject(instance):
     """
     """
     return True
 
+
 def guard_release(instance):
     """
     """
     return True
+
 
 def guard_released(instance):
     """
     """
     return True
 
+
 def guard_reserve_for_stability_testing(instance):
     """
     """
     return True
+
 
 def guard_residual(instance):
     """
     """
     return True
 
+
 def guard_retain_ial(instance):
     """
     """
     return True
+
 
 def guard_retain_usa(instance):
     """
     """
     return True
 
+
 def guard_use_for_release_testing(instance):
     """
     """
     return True
+
 
 def guard_use_for_stability_testing(instance):
     """
     """
     return True
 
+
 def guard_use_for_training(instance):
     """
     """
     return True
+
 
 def guard_use_for_validation(instance):
     """
     """
     return True
 
+
 def guard_used_qc_fail(instance):
     """
     """
     return True
+
 
 def guard_used_qc_pass(instance):
     """
