38210021cf0386a5a781383ea5732bfe8a38c6cd
Campbell <campbell@bikalabs.com>
2017-08-27 14:40:00 +0200

    Add event and guard stubs for ichiplot workflow

---
+++ immunarray/lims/workflow/ichiplot/events.py
@@ -0,0 +1,16 @@
+def after_release(self, instance):
+    """
+    """
+    pass
+
+
+def after_quarantine(self, instance):
+    """
+    """
+    pass
+
+
+def after_close(self, instance):
+    """
+    """
+    pass
---
+++ immunarray/lims/workflow/ichiplot/guards.py
@@ -0,0 +1,16 @@
+def guard_release(instance):
+    """
+    """
+    return True
+
+
+def guard_quarantine(instance):
+    """
+    """
+    return True
+
+
+def guard_close(instance):
+    """
+    """
+    return True
