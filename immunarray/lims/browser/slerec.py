1d6fa523d2828118028846f42bca493cc8bca14a
Campbell <campbell@bikalabs.com>
2017-08-27 15:49:31 +0200

    Syntax error in stub slerec.py

--- immunarray/lims/browser/slerec.py
+++ immunarray/lims/browser/slerec.py
@@ -1,6 +1,6 @@
 from Products.Five.browser import BrowserView
 from plone import api
-from immunarray.lims.interfaces.clinicalsample import IClinicalSample
+
 
 class RecView(BrowserView):
     """ View that pulls data from multiple content objects
@@ -9,6 +9,5 @@ class RecView(BrowserView):
     def usn(self):
         results = []
         brains = api.content.find(context=self.context, portal_type="title")
-        for brain in brains
+        for brain in brains:
             title = brain.getObject()
-            results.append().
