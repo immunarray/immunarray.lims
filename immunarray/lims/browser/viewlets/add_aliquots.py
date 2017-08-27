59d403f033a44b3fb82caae2f0109d1c24ea91e1
Campbell <campbell@bikalabs.com>
2017-08-27 23:39:28 +0200

    WIP: add-aliquots viewlet

--- immunarray/lims/browser/viewlets/add_aliquots.py
+++ immunarray/lims/browser/viewlets/add_aliquots.py
@@ -25,19 +25,17 @@ class AddAliquotsViewlet(ViewletBase):
         # self.request.response.redirect(bulk.absolute_url())
 
 
-class AddAliquotsViewletAJAXHandler(BrowserView):
+class AddAliquotsViewletAJAXFeedback(BrowserView):
     """When any input in the add-aliquots viewlet is changed, this
     view returns feedback to the viewlet.
     """
 
     def __call__(self):
 
-        result = {
-            'success': False,
-            'feedback': 'Error.',
-        }
+        result = {'success': False, 'feedback': 'Error.'}
 
-        # validate aliquot_volume
+        # validate aliquot_volume.
+        # After this block, aliquot_volume WILL be defined.
         try:
             aliquot_volume = self.request.form.get('aliquot_volume', '')
             aliquot_volume = int(aliquot_volume)
@@ -50,6 +48,8 @@ class AddAliquotsViewletAJAXHandler(BrowserView):
 
         # validate aliquot_count.  If no count is entered, calculate
         # from parent's remaining_volume / entered aliquot_volume.
+        # after this block, aliquot_count and remaining_volume WILL be defined.
+        parent_volume = self.context.remaining_volume
         try:
             aliquot_count = self.request.form.get('aliquot_count', '')
             aliquot_count = int(aliquot_count)
@@ -57,26 +57,30 @@ class AddAliquotsViewletAJAXHandler(BrowserView):
             result['feedback'] = 'Aliquot count must be a whole number.'
             return json.dumps(result)
         if not aliquot_count:
-            dm = divmod(self.context.remaining_volume, aliquot_volume)
-            aliquot_count, remaining_volume = dm
+            dm = divmod(parent_volume, aliquot_volume)
+            aliquot_count = dm[0]
+            # this aliquot_count will be set in the viewlet's input element.
             result['aliquot_count'] = aliquot_count
+            required_volume = aliquot_count * aliquot_volume
+            remaining_volume = dm[1]
         else:
             required_volume = aliquot_count * aliquot_volume
-            if required_volume > self.context.remaining_volume:
+            if required_volume > parent_volume:
                 result['feedback'] = \
                     "Remaining volume is not sufficient for requested aliquots."
                 return json.dumps(result)
-            remaining_volume = self.context.remaining_volume - required_volume
+            remaining_volume = parent_volume - required_volume
 
-        # brains = find(object_provides=[IQCAliquot.__identifier__,
-        #                                IRandDAliquot.__identifier__],
-        #               veracis_id=self.context.veracis_id,
-        #               sort_on='id',
-        #               sort_order='reverse',
-        #               limit=1)
+        # Get the sequence start for the new aliquots which will be created.
         brains = find(object_provides=[IQCAliquot.__identifier__,
                                        IRandDAliquot.__identifier__],
-                      path={'query': self.context.getPhysicalPath(),
-                            'level': '1',
-                            'depth': '1',})
+                      veracis_id=self.context.veracis_id)
         sequence_start = len(brains)
+
+        aliquot_type = self.request.form['aliquot_type']
+        result['feedback'] = """{aliquot_count} {aliquot_type} aliquots will 
+        be created, using {required_volume} uL of {parent_volume} uL 
+        remaining on {self.context.id}, and leavning {remaining_volume} 
+        unallocated.""".format(**locals())
+        result['success'] = True
+        return json.dumps(result)
