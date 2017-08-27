dfc5ec1bb6c05dbd1232f580693ba2af66844076
Campbell <campbell@bikalabs.com>
2017-08-27 23:03:10 +0200

    Add veracis_id property to QCAliquot and RandDAliquot.

--- immunarray/lims/content/qcaliquot.py
+++ immunarray/lims/content/qcaliquot.py
@@ -1,6 +1,16 @@
 from immunarray.lims.content.abstractaliquot import AbstractAliquot
+from immunarray.lims.interfaces.sample import ISample
 
 
 class QCAliquot(AbstractAliquot):
     def __init__(self, *args, **kwargs):
         super(QCAliquot, self).__init__(*args, **kwargs)
+
+    @property
+    def veracis_id(self):
+        """Return the veracis_id of the sample this aliquot originated from.
+        """
+        parent = self.aq_parent
+        while not ISample.providedBy(parent):
+            parent = self.aq_parent
+        return parent.veracis_id
--- immunarray/lims/content/randdaliquot.py
+++ immunarray/lims/content/randdaliquot.py
@@ -4,3 +4,12 @@ from immunarray.lims.content.abstractaliquot import AbstractAliquot
 class RandDAliquot(AbstractAliquot):
     def __init__(self, *args, **kwargs):
         super(RandDAliquot, self).__init__(*args, **kwargs)
+
+    @property
+    def veracis_id(self):
+        """Return the veracis_id of the sample this aliquot originated from.
+        """
+        parent = self.aq_parent
+        while not ISample.providedBy(parent):
+            parent = self.aq_parent
+        return parent.veracis_id
