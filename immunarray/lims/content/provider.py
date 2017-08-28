cc443a479656ffc9633b658e5d3778ea364765ed
immunarray <justin.pitts@immunarray.com>
2017-08-28 11:20:53 -0400

    add set_ for first_name and last_name to update title on change of either field

--- immunarray/lims/content/provider.py
+++ immunarray/lims/content/provider.py
@@ -4,3 +4,12 @@ from . import BaseContainer
 class Provider(BaseContainer):
     def __init__(self, *args, **kwargs):
         super(Provider, self).__init__(*args, **kwargs)
+
+    def set_last_name(self, value):
+        fullname = self.first_name + " " + value
+        self.setTitle(fullname)
+
+    def set_first_name(self, value):
+        fullname = value + " " + self.last_name
+        self.setTitle(fullname)
+
