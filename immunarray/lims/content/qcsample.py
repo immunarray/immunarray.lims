fc01299ec342d6603894650c9a99292d33a8601f
immunarray <justin.pitts@immunarray.com>
2017-08-28 12:58:26 -0400

    getter, setter property update for qcsample

--- immunarray/lims/content/qcsample.py
+++ immunarray/lims/content/qcsample.py
@@ -9,10 +9,29 @@ class QCSample(AbstractSample):
     def __init__(self, *args, **kwargs):
         super(QCSample, self).__init__(*args, **kwargs)
 
-    def set_source_id_one(self, value):
-        title = self.veracis_id + " " + value
-        self.setTitle(title)
+    # def set_source_id_one(self, value):
+    #     title = self.veracis_id + " " + value
+    #     self.setTitle(title)
+    #
+    # def set_veracis_id(self, value):
+    #     title = value + " " + self.source_id_one
+    #     self.setTitle(title)
+    @property
+    def veracis_id(self):
+        return getattr(self, "_veracis_id", "")
 
-    def set_veracis_id(self, value):
-        title = value + " " + self.source_id_one
-        self.setTitle(title)
+    @veracis_id.setter
+    def veracis_id(self, value):
+        self._veracis_id = value
+        temp = getattr(self, "_source_id_one", "")
+        self.setTitle(value + " - " + temp)
+
+    @property
+    def source_id_one(self):
+        return getattr(self, "_source_id_one", "")
+
+    @source_id_one.setter
+    def source_id_one(self, value):
+        self._source_id_one = value
+        temp = getattr(self, "_veracis_id", "")
+        self.setTitle(temp + " - " + value)
