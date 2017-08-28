67b6ddca499cedf42a5750a17bac438710e5bc5d
immunarray <justin.pitts@immunarray.com>
2017-08-28 13:05:05 -0400

    removed commented code

--- immunarray/lims/content/qcsample.py
+++ immunarray/lims/content/qcsample.py
@@ -9,13 +9,6 @@ class QCSample(AbstractSample):
     def __init__(self, *args, **kwargs):
         super(QCSample, self).__init__(*args, **kwargs)
 
-    # def set_source_id_one(self, value):
-    #     title = self.veracis_id + " " + value
-    #     self.setTitle(title)
-    #
-    # def set_veracis_id(self, value):
-    #     title = value + " " + self.source_id_one
-    #     self.setTitle(title)
     @property
     def veracis_id(self):
         return getattr(self, "_veracis_id", "")
