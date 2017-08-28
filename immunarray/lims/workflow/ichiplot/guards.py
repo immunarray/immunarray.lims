6ee07031f1bcd6737a0d466fa2813c69f814313d
immunarray <justin.pitts@immunarray.com>
2017-08-28 14:09:41 -0400

    add guard on ichiplot release based on presence of temp log, cofa, batch_release, and gal_file

--- immunarray/lims/workflow/ichiplot/guards.py
+++ immunarray/lims/workflow/ichiplot/guards.py
@@ -1,6 +1,13 @@
 def guard_release(instance):
     """
     """
+
+    if not all([instance.temp_log,
+                instance.cofa,
+                instance.batch_release,
+                instance.gal_file]):
+        return False
+
     return True
 
 
