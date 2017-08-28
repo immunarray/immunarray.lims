ab0b42ed5ec682133ba4769618a7cee1ae0a0778
immunarray <justin.pitts@immunarray.com>
2017-08-28 11:21:12 -0400

    add set_ for product_name and for lot_number to set title on update of either of the fields

--- immunarray/lims/content/qcsample.py
+++ immunarray/lims/content/qcsample.py
@@ -8,3 +8,11 @@ class QCSample(AbstractSample):
 
     def __init__(self, *args, **kwargs):
         super(QCSample, self).__init__(*args, **kwargs)
+
+    def set_source_id_one(self, value):
+        title = self.veracis_id + " " + value
+        self.setTitle(title)
+
+    def set_veracis_id(self, value):
+        title = value + " " + self.source_id_one
+        self.setTitle(title)
