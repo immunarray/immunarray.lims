212153fd89846f9afef43ba0ca15567f82fc13ef
immunarray <justin.pitts@immunarray.com>
2017-08-28 11:18:53 -0400

    add set_ for product_name and for lot_number to set title on update of either of the fields

--- immunarray/lims/content/material.py
+++ immunarray/lims/content/material.py
@@ -4,3 +4,12 @@ from . import BaseContainer
 class Material(BaseContainer):
     def __init__(self, *args, **kwargs):
         super(Material, self).__init__(*args, **kwargs)
+
+    def set_product_name(self, value):
+        import pdb;pdb.set_trace()
+        title = value + " - " + self.lot_number
+        self.setTitle(title)
+
+    def set_lot_number(self, value):
+        title = self.product_name + " - " + value
+        self.setTitle(title)
