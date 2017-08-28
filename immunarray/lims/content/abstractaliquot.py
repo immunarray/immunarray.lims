bd07dcd47e42844250ab8d1fe2fbb8020344c094
Campbell <campbell@bikalabs.com>
2017-08-28 02:16:51 +0200

    Fix AbstractAliquot __setter__ for general use.

--- immunarray/lims/content/abstractaliquot.py
+++ immunarray/lims/content/abstractaliquot.py
@@ -8,6 +8,7 @@ class AbstractAliquot(Container):
         super(AbstractAliquot, self).__init__(*args, **kwargs)
 
     def __setattr__(self, key, value):
+        super(AbstractAliquot, self).__setattr__(key, value)
         if key == 'aliquot_type':
             if value == 'Bulk':
                 alsoProvides(self, IBulkAliquot)
