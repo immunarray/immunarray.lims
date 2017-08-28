f5f3efc18ac5bd131f93124487cff398962bb1ba
immunarray <justin.pitts@immunarray.com>
2017-08-28 12:41:42 -0400

    getter, setter property update for aliquot

--- immunarray/lims/content/abstractaliquot.py
+++ immunarray/lims/content/abstractaliquot.py
@@ -1,13 +1,20 @@
 from immunarray.lims.interfaces import IWorkingAliquot, IBulkAliquot
-from . import BaseContainer
 from zope.interface import alsoProvides
+
+from . import BaseContainer
 
 
 class AbstractAliquot(BaseContainer):
     def __init__(self, *args, **kwargs):
         super(AbstractAliquot, self).__init__(*args, **kwargs)
 
-    def set_aliquot_type(self, value):
+    @property
+    def aliquot_type(self):
+        return getattr(self, "_aliquot_type", "")
+
+    @aliquot_type.setter
+    def aliquot_type(self, value):
+        self._aliquot_type = value
         if value == 'Bulk':
             alsoProvides(self, IBulkAliquot)
         else:
