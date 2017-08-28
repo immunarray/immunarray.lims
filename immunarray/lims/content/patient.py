91ca46cf4e821555528cef85d91001b31354fe89
immunarray <justin.pitts@immunarray.com>
2017-08-28 11:19:33 -0400

    remove behavior code from interface

--- immunarray/lims/content/patient.py
+++ immunarray/lims/content/patient.py
@@ -4,3 +4,11 @@ from . import BaseContainer
 class Patient(BaseContainer):
     def __init__(self, *args, **kwargs):
         super(Patient, self).__init__(*args, **kwargs)
+
+    def set_last_name(self, value):
+        fullname = self.first_name + " " + value
+        self.setTitle(fullname)
+
+    def set_first_name(self, value):
+        fullname = value + " " + self.last_name
+        self.setTitle(fullname)
