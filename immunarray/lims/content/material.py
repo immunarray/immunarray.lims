a0cdf39bfad1c8d184d78d234443341ccd1dbb50
immunarray <justin.pitts@immunarray.com>
2017-08-28 12:33:41 -0400

    getter, setter property update for title of material

--- immunarray/lims/content/material.py
+++ immunarray/lims/content/material.py
@@ -5,11 +5,22 @@ class Material(BaseContainer):
     def __init__(self, *args, **kwargs):
         super(Material, self).__init__(*args, **kwargs)
 
-    def set_product_name(self, value):
-        import pdb;pdb.set_trace()
-        title = value + " - " + self.lot_number
-        self.setTitle(title)
+    @property
+    def product_name(self):
+        return getattr(self, "_product_name", "")
 
-    def set_lot_number(self, value):
-        title = self.product_name + " - " + value
-        self.setTitle(title)
+    @product_name.setter
+    def product_name(self, value):
+        self._product_name = value
+        temp = getattr(self, "_lot_number", "")
+        self.setTitle(value + " - " + temp)
+
+    @property
+    def lot_number(self):
+        return getattr(self, "_lot_number", "")
+
+    @lot_number.setter
+    def lot_number(self, value):
+        self._lot_number = value
+        temp = getattr(self, "_product_name", "")
+        self.setTitle(temp + " - " + value)
