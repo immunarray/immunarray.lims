ec2bef7b4c5de2941bc96222d4af7ad92c580546
immunarray <justin.pitts@immunarray.com>
2017-08-29 18:46:20 -0400

    events and guards for material workflow inital

---
+++ immunarray/lims/workflow/material/events.py
@@ -0,0 +1,18 @@
+def after_quarantine(instance):
+    """
+    """
+
+
+def after_release(instance):
+    """
+    """
+
+
+def after_put_in_use(instance):
+    """
+    """
+
+
+def after_close(instance):
+    """
+    """
---
+++ immunarray/lims/workflow/material/guards.py
@@ -0,0 +1,22 @@
+def guard_quarantine(instance):
+    """
+    """
+    return True
+
+
+def guard_release(instance):
+    """
+    """
+    return True
+
+
+def guard_put_in_use(instance):
+    """
+    """
+    return True
+
+
+def guard_close(instance):
+    """
+    """
+    return True
