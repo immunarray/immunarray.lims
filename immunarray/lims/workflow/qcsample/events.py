e7b5df2fed59b714e1e47cc2998d38e84d8cf90b
Campbell <campbell@bikalabs.com>
2017-08-29 12:16:32 +0200

    Add QCSample Workflow definition, guards and events

---
+++ immunarray/lims/workflow/qcsample/events.py
@@ -0,0 +1,26 @@
+from plone.api.content import transition, get_state
+
+
+def after_consume(instance):
+    """
+    """
+
+
+def after_reject(instance):
+    """
+    """
+
+
+def after_release(instance):
+    """
+    """
+
+
+def after_review(instance):
+    """
+    """
+
+
+def after_use(instance):
+    """
+    """
---
+++ immunarray/lims/workflow/qcsample/guards.py
@@ -0,0 +1,28 @@
+def guard_consume(instance):
+    """
+    """
+    return True
+
+
+def guard_reject(instance):
+    """
+    """
+    return True
+
+
+def guard_release(instance):
+    """
+    """
+    return True
+
+
+def guard_review(instance):
+    """
+    """
+    return True
+
+
+def guard_use(instance):
+    """
+    """
+    return True
