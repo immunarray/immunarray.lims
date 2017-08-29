adf72eb37663aa4bb1bf6ca9445bf0d0e38f76bf
immunarray <justin.pitts@immunarray.com>
2017-08-29 19:17:12 -0400

    assay request workflow events, guards and definitions

---
+++ immunarray/lims/workflow/assayrequest/events.py
@@ -0,0 +1,33 @@
+def after_order(instance):
+    """
+    """
+
+
+def after_to_test(instance):
+    """
+    """
+
+
+def after_queue(instance):
+    """
+    """
+
+
+def after_re_run(instance):
+    """
+    """
+
+
+def after_result(instance):
+    """
+    """
+
+
+def after_report(instance):
+    """
+    """
+
+
+def after_reject(instance):
+    """
+    """
---
+++ immunarray/lims/workflow/assayrequest/guards.py
@@ -0,0 +1,33 @@
+def guard_order(instance):
+    """
+    """
+
+
+def guard_to_test(instance):
+    """
+    """
+
+
+def guard_queue(instance):
+    """
+    """
+
+
+def guard_re_run(instance):
+    """
+    """
+
+
+def guard_result(instance):
+    """
+    """
+
+
+def guard_report(instance):
+    """
+    """
+
+
+def guard_reject(instance):
+    """
+    """
