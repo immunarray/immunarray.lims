6e1e06c7e97971db5aa7bd02d9523a03e5ca4482
Campbell <campbell@bikalabs.com>
2017-08-30 14:55:16 +0200

    ichiplot.lot_id -> title

--- immunarray/lims/content/ichiplot.py
+++ immunarray/lims/content/ichiplot.py
@@ -4,3 +4,12 @@ from . import BaseContainer
 class iChipLot(BaseContainer):
     def __init__(self, *args, **kwargs):
         super(iChipLot, self).__init__(*args, **kwargs)
+
+    @property
+    def lot_id(self):
+        return getattr(self, "_lot_id", "")
+
+    @lot_id.setter
+    def lot_id(self, value):
+        self._lot_id = value
+        self.setTitle(value)
