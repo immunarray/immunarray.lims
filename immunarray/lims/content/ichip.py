52762060ec7b2d7f1fd9d608574bd0a4771f22d8
immunarray <justin.pitts@immunarray.com>
2017-08-30 18:08:05 -0400

    ichip title in content

--- immunarray/lims/content/ichip.py
+++ immunarray/lims/content/ichip.py
@@ -4,3 +4,12 @@ from . import BaseContainer
 class iChip(BaseContainer):
     def __init__(self, *args, **kwargs):
         super(iChip, self).__init__(*args, **kwargs)
+
+    @property
+    def ichip_id(self):
+        return getattr(self, "_ichip_id", "")
+
+    @ichip_id.setter
+    def ichip_id(self, value):
+        self._ichip_id = value
+        self.setTitle(value)
