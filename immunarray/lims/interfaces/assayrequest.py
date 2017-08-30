3408b0729eaf4d3796291f45209afbda31a9612b
immunarray <justin.pitts@immunarray.com>
2017-08-30 13:47:59 -0400

    update to BaseModel, and needed imports for alsoprovides

--- immunarray/lims/interfaces/assayrequest.py
+++ immunarray/lims/interfaces/assayrequest.py
@@ -4,14 +4,17 @@ from zope import schema
 import datetime
 from immunarray.lims import messageFactory as _
 from immunarray.lims.vocabularies.ichipassay import IChipAssayListVocabulary
+from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel import model
+from immunarray.lims.interfaces import BaseModel
+from zope.interface import alsoProvides
 
 
 def currentDate():
     return datetime.datetime.now().date()
 
 
-class IAssayRequest(model.Schema):
+class IAssayRequest(BaseModel):
     """Object that will be attached to sample.  It will be the instance object
     that has a workflow that can be tracked in the LIMS.
     """
@@ -19,8 +22,8 @@ class IAssayRequest(model.Schema):
         title=_(u"Assay Name"),
         description=_(u"Assay Name"),
         source=IChipAssayListVocabulary,
-        required=True
-    ),
+        required=True,
+    )
 
     date_ordered = schema.Date(
         title=_(u"Date QC Sample was added to LIMS"),
@@ -35,12 +38,6 @@ class IAssayRequest(model.Schema):
         required=False,
     )
 
-    date_billed = schema.Date(
-        title=_(u"Assay Billed on Date"),
-        description=_(u"Assay Billed on Date"),
-        required=False,
-    )
-
     aliquot_evaluated = schema.TextLine(
         title=_(u"Aliquot ID Used For Assay Request"),
         description=_(u"Aliquot ID Used For Assay Request"),
@@ -52,3 +49,5 @@ class IAssayRequest(model.Schema):
         description=_(u"Any Notes or Comments About the Assay Request"),
         required=False,
     )
+
+alsoProvides(IAssayRequest, IFormFieldProvider)
