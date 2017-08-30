3e1a3a46c2a6baac2b7301d0855bd313affb10fa
immunarray <justin.pitts@immunarray.com>
2017-08-30 11:24:42 -0400

    update to allow for addition of assayrequest, assaybillingrequest to created clinical sampels, and add assayreqeust to randd and qc samples (longterm use needed)

--- immunarray/lims/subscribers/sample.py
+++ immunarray/lims/subscribers/sample.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from immunarray.lims.permissions import AddClinicalAliquot, AddClinicalSample, \
-    AddRandDSample, AddRandDAliquot, AddQCAliquot, AddQCSample, AddNCE
+    AddRandDSample, AddRandDAliquot, AddQCAliquot, AddQCSample, AddNCE,AddAssayRequest,AddAssayBillingRequest
 from bika.lims.permissions import disallow_default_contenttypes
 from bika.lims.utils.limsroot import getLims
 
@@ -8,6 +8,8 @@ def ClinicalSampleAdded(clinicalsample, event):
     """A new Clinical Sample has been created!
     """
     clinicalsample.manage_permission(AddClinicalAliquot, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
+    clinicalsample.manage_permission(AddAssayBillingRequest, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
+    clinicalsample.manage_permission(AddAssayRequest, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
     clinicalsample.manage_permission(AddNCE, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
     # Don't allow samples to be nested in each other!
     clinicalsample.manage_permission(AddQCSample, [], 0)
@@ -21,6 +23,7 @@ def ClinicalSampleAdded(clinicalsample, event):
 
 def RandDSampleAdded(randdsample, event):
     randdsample.manage_permission(AddRandDAliquot, ['Manager', 'LabManager', 'LabClerk', 'Owner','RandDLabClerk', 'RandDLabManager'], 0)
+    randdsample.manage_permission(AddAssayRequest, ['Manager', 'LabManager', 'LabClerk', 'Owner','RandDLabClerk', 'RandDLabManager'], 0)
     randdsample.manage_permission(AddNCE, ['Manager', 'LabManager', 'LabClerk', 'Owner','RandDLabClerk', 'RandDLabManager'], 0)
     # Don't allow samples to be nested in each other!
     randdsample.manage_permission(AddClinicalSample, [], 0)
@@ -35,6 +38,7 @@ def RandDSampleAdded(randdsample, event):
 
 def QCSampleAdded(qcsample, event):
     qcsample.manage_permission(AddQCAliquot, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
+    qcsample.manage_permission(AddAssayRequest, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
     qcsample.manage_permission(AddNCE, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
     # Don't allow samples to be nested in each other!
     qcsample.manage_permission(AddRandDSample, [], 0)
