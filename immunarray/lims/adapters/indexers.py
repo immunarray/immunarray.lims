1630a0df4554cd0a3b50072de045d88439b4e970
immunarray <justin.pitts@immunarray.com>
2017-08-30 18:06:57 -0400

    add to deal with date going to plone tool

--- immunarray/lims/adapters/indexers.py
+++ immunarray/lims/adapters/indexers.py
@@ -1,6 +1,8 @@
 from immunarray.lims.interfaces.aliquot import IAliquot
 from immunarray.lims.interfaces.ichipassay import IiChipAssay
+from immunarray.lims.interfaces.ichiplot import IiChipLot
 from plone.indexer import indexer
+import datetime as dt
 
 
 @indexer(IAliquot)
@@ -14,3 +16,9 @@ def ichipassay_Type(instance):
     """Append the ichip-assay (status) to the Type for iChipAssays.
     """
     return "{} ({})".format(instance.Type(), instance.status)
+
+@indexer(IiChipLot)
+def ichiplot_expires(instance):
+    """iChipLot ichip_lot_expiration_date populates 'expires' index
+    """
+    return dt.datetime.combine(instance.ichip_lot_expiration_date, dt.time.min)
