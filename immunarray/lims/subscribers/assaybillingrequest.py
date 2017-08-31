4a906e1071fa34cd08641a2ab4382429bf6ab04b
immunarray <justin.pitts@immunarray.com>
2017-08-31 17:22:27 -0400

    update assaybilling request to have needed fields

--- immunarray/lims/subscribers/assaybillingrequest.py
+++ immunarray/lims/subscribers/assaybillingrequest.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 from bika.lims.permissions import disallow_default_contenttypes
-from immunarray.lims.permissions import AddAssayBillingRequest, AddAssayRequest
+from immunarray.lims.permissions import AddAssayBillingRequest, AddAssayRequest, \
+    AddNCE, AddClinicalAliquot
 
 
-def AssayRequest(assayrequest, event):
-    """A new assay request been created!
+def AssayBillingRequest(assaybillingrequest, event):
+    """A new assay billing request been created!
     """
-    assayrequest.manage_permission(AddAssayBillingRequest, ['LabManager', 'BillingExec'], 0)
+    assaybillingrequest.manage_permission(AddNCE, ['LabManager', 'LabClerk','BillingExec'], 0)
     # Don't allow assay request to be nested in each other!
-    assayrequest.manage_permission(AddAssayRequest, [], 0)
-    disallow_default_contenttypes(assayrequest)
+    assaybillingrequest.manage_permission(AddAssayRequest, [], 0)
+    assaybillingrequest.manage_permission(AddClinicalAliquot, [], 0)
+    assaybillingrequest.manage_permission(AddAssayBillingRequest, [], 0)
+
+    disallow_default_contenttypes(assaybillingrequest)
