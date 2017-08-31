f0bb7d55d1f1895ada130fc74f5f1bd9664b9d9b
immunarray <justin.pitts@immunarray.com>
2017-08-31 17:23:01 -0400

    permissions for assayrequest

--- immunarray/lims/subscribers/assayrequest.py
+++ immunarray/lims/subscribers/assayrequest.py
@@ -1,43 +1,16 @@
 # -*- coding: utf-8 -*-
-from immunarray.lims.permissions import AddClinicalAliquot, AddClinicalSample, \
-    AddRandDSample, AddRandDAliquot, AddQCAliquot, AddQCSample
 from bika.lims.permissions import disallow_default_contenttypes
-from bika.lims.utils.limsroot import getLims
+from immunarray.lims.permissions import AddAssayBillingRequest, AddAssayRequest, \
+    AddNCE, AddClinicalAliquot
 
-def ClinicalAliquotAdded(clinicalaliquot, event):
-    """A new Clinical Sample has been created!
+
+def AssayRequest(assayrequest, event):
+    """A new assay request been created!
     """
-    clinicalaliquot.manage_permission(AddClinicalAliquot, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
-    # Don't allow samples to be nested in each other!
-    clinicalaliquot.manage_permission(AddQCSample, [], 0)
-    clinicalaliquot.manage_permission(AddRandDSample, [], 0)
-    clinicalaliquot.manage_permission(AddClinicalSample, [], 0)
-    # Don't allow other aliquots to be added (only R&D Aliquots should be added)
-    clinicalaliquot.manage_permission(AddRandDAliquot, [], 0)
-    clinicalaliquot.manage_permission(AddQCAliquot, [], 0)
-    disallow_default_contenttypes(clinicalaliquot)
+    assayrequest.manage_permission(AddAssayBillingRequest, ['LabManager', 'BillingExec'], 0)
+    assayrequest.manage_permission(AddNCE, ['LabManager', 'BillingExec', 'LabClerk'], 0)
+    # Don't allow assay request to be nested in each other!
+    assayrequest.manage_permission(AddAssayRequest, [], 0)
+    assayrequest.manage_permission(AddClinicalAliquot, [], 0)
 
-
-def RandDAliquotAdded(randdaliquot, event):
-    randdaliquot.manage_permission(AddRandDAliquot, ['Manager', 'LabManager', 'LabClerk', 'Owner','RandDLabClerk', 'RandDLabManager'], 0)
-    # Don't allow samples to be nested in each other!
-    randdaliquot.manage_permission(AddClinicalSample, [], 0)
-    randdaliquot.manage_permission(AddRandDSample, [], 0)
-    randdaliquot.manage_permission(AddQCSample, [], 0)
-    # Don't allow other aliquots to be added (only R&D Aliquots should be added)
-    randdaliquot.manage_permission(AddClinicalAliquot, [], 0)
-    randdaliquot.manage_permission(AddQCAliquot, [], 0)
-    disallow_default_contenttypes(randdaliquot)
-
-
-def QCAliquotAdded(qcaliquot, event):
-
-    qcaliquot.manage_permission(AddQCAliquot, ['Manager', 'LabManager', 'LabClerk', 'Owner'], 0)
-    # Don't allow samples to be nested in each other!
-    qcaliquot.manage_permission(AddRandDSample, [], 0)
-    qcaliquot.manage_permission(AddQCSample, [], 0)
-    qcaliquot.manage_permission(AddClinicalSample, [], 0)
-    # Don't allow other aliquots to be added (only QC Aliquots should be added)
-    qcaliquot.manage_permission(AddClinicalAliquot, [], 0)
-    qcaliquot.manage_permission(AddRandDAliquot, [], 0)
-    disallow_default_contenttypes(qcaliquot)
+    disallow_default_contenttypes(assayrequest)
