6ba1cc79da7273c3d4156e643e57a207611537b2
Campbell <campbell@bikalabs.com>
2017-09-01 03:23:32 +0200

    Amount field.  Based on IDecimal.

---
+++ immunarray/lims/fields/amount.py
@@ -0,0 +1,31 @@
+from decimal import Decimal as D, InvalidOperation
+
+from zope.schema import Decimal, ValidationError
+from zope.schema.interfaces import IDecimal
+
+
+class IAmount(IDecimal):
+    """Field containing a unicode string representing a Magnitude value.
+    """
+
+
+class DecimalRequired(ValidationError):
+    __doc__ = "A decimal value is required."
+
+
+class NonNegativeValueRequired(ValidationError):
+    __doc__ = "A non-negative value is required."
+
+
+class Amount(Decimal):
+    def validate(self, value):
+
+        if value:
+            try:
+                D(value)
+            except InvalidOperation:
+                raise DecimalRequired
+            if value < 0:
+                raise NonNegativeValueRequired
+
+        return super(Amount, self).validate(value)
