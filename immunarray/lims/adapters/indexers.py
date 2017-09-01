fb27368b9ff2afc2197a1c22741c3627530ab296
Campbell <campbell@bikalabs.com>
2017-09-01 03:16:37 +0200

    set 'remaining_amount' into the 'remaining_volume' for materials and solutions

--- immunarray/lims/adapters/indexers.py
+++ immunarray/lims/adapters/indexers.py
@@ -1,6 +1,8 @@
 from immunarray.lims.interfaces.aliquot import IAliquot
 from immunarray.lims.interfaces.ichipassay import IiChipAssay
 from immunarray.lims.interfaces.ichiplot import IiChipLot
+from immunarray.lims.interfaces.material import IMaterial
+from immunarray.lims.interfaces.solution import ISolution
 from plone.indexer import indexer
 import datetime as dt
 
@@ -22,3 +24,15 @@ def ichiplot_expires(instance):
     """iChipLot ichip_lot_expiration_date populates 'expires' index
     """
     return dt.datetime.combine(instance.ichip_lot_expiration_date, dt.time.min)
+
+@indexer(IMaterial)
+def material_remaining_volume(instance):
+    """populate "remaining_volume" index with "remaining_amount" field value
+    """
+    return instance.remaining_amount
+
+@indexer(ISolution)
+def solution_remaining_volume(instance):
+    """populate "remaining_volume" index with "remaining_amount" field value
+    """
+    return instance.remaining_amount
