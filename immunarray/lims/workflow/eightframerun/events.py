3fba7dd80a7cbc852faeb207ba22cd018ab4d970
immunarray <justin.pitts@immunarray.com>
2017-09-03 12:07:02 -0400

    commercailrun_workflow

--- immunarray/lims/workflow/eightframerun/events.py
+++ immunarray/lims/workflow/eightframerun/events.py
@@ -1,7 +1,28 @@
-def after_activate(instance):
+def after_queue(instance):
     """
     """
 
-def after_retire(instance):
+def after_in_process(instance):
     """
     """
+def after_result(instance):
+    """
+    """
+
+def after_report(instance):
+    """
+    """
+def after_wet_work_done(instance):
+    """
+    """
+
+def after_scanning(instance):
+    """
+    """
+def after_abort_run(instance):
+    """
+    """
+def after_cancell_run(instance):
+    """
+    """
+
--- immunarray/lims/workflow/eightframerun/guards.py
+++ immunarray/lims/workflow/eightframerun/guards.py
@@ -1,9 +1,35 @@
-def guard_activate(instance):
+def gurard_queue(instance):
     """
     """
     return True
 
-def guard_retire(instance):
+def gurard_in_process(instance):
+    """
+    """
+    return True
+def gurard_result(instance):
+    """
+    """
+    return True
+
+def gurard_report(instance):
+    """
+    """
+    return True
+def gurard_wet_work_done(instance):
+    """
+    """
+    return True
+
+def gurard_scanning(instance):
+    """
+    """
+    return True
+def gurard_abort_run(instance):
+    """
+    """
+    return True
+def gurard_cancell_run(instance):
     """
     """
     return True
