7aa2a6da415f87355b6ea666c6c65e53722fee1e
immunarray <justin.pitts@immunarray.com>
2017-09-04 12:10:48 -0400

    rack setTitle

--- immunarray/lims/content/rack.py
+++ immunarray/lims/content/rack.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
+from Interface import Interface
+from immunarray.lims.interfaces.storage import IRack
+from zope.component import adapter
+from zope.interface import implementer
+
 from . import BaseContainer
 
-
+@implementer(IRack)
+@adapter(Interface)
 class Rack(BaseContainer):
     def __init__(self, *args, **kwargs):
         super(Rack, self).__init__(*args, **kwargs)
+
+    @property
+    def rack_name(self):
+        return getattr(self, "_rack_name", "")
+
+    @rack_name.setter
+    def rack_name(self, value):
+        self._rack_name = value
+        self.setTitle(value)
