949845ef837f0a5bc8aab47031cea5b2d45dc601
immunarray <justin.pitts@immunarray.com>
2017-09-03 21:38:35 -0400

    remove default content

--- immunarray/lims/subscribers/material.py
+++ immunarray/lims/subscribers/material.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from bika.lims.permissions import disallow_default_contenttypes
 from immunarray.lims.interfaces.material import IMaterial
 from immunarray.lims.permissions import AddMaterial
 from plone.api.portal import get_tool
@@ -22,3 +23,4 @@ def MaterialModified(instance, event):
     """A new material has been created!
     """
     instance.manage_permission(AddMaterial, [], 0)
+    disallow_default_contenttypes(instance)
