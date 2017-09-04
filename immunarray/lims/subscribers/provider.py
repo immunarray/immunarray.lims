6592dd0128ec9d2fd235ff9d233f48c58dea5456
immunarray <justin.pitts@immunarray.com>
2017-09-04 17:34:46 -0400

    provider subscriber

---
+++ immunarray/lims/subscribers/provider.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from bika.lims.permissions import disallow_default_contenttypes
+from immunarray.lims.interfaces.material import IMaterial
+from immunarray.lims.permissions import AddMaterial, AddSite
+from plone.api.portal import get_tool
+
+
+
+def SiteAdded(instance, event):
+    """A new material has been created!
+    """
+    instance.manage_permission(AddSite, [], 0)
+    disallow_default_contenttypes(instance)
