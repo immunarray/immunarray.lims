0b1ebf6591aad4220afff06f612b14ae43fa102a
immunarray <justin.pitts@immunarray.com>
2017-09-05 18:36:26 -0400

    bottle number for materials

--- immunarray/lims/content/__init__.py
+++ immunarray/lims/content/__init__.py
@@ -1,6 +1,28 @@
+from immunarray.lims import logger
+from plone.api.content import find
 from plone.dexterity.content import Container
+from zope.interface import implements
+from zope.schema._bootstrapinterfaces import IContextAwareDefaultFactory
 
 
 class BaseContainer(Container):
     """
     """
+
+class AssignBottleNumber():
+    """make bottle number for new materials
+    """
+    implements(IContextAwareDefaultFactory)
+
+    def __init__(self):
+        pass
+
+    def __call__(self, context):
+        """Pull all Veracis IDs for R&D and QC samples and get the next one.
+        """
+        brains = find(portal_type=context.REQUEST.URL.split('++')[-1])
+        if not brains:
+            return 1
+        return max([b.getObject().bottle_number for b in brains]) + 1
+
+
