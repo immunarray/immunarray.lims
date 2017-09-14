092a25713a9dfd38f041a23c2fcafaef015b8d91
immunarray <justin.pitts@immunarray.com>
2017-09-14 14:03:02 -0400

    QCBox object

---
+++ immunarray/lims/content/qcbox.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from . import BaseContainer
+
+
+class RandDBox(BaseContainer):
+    def __init__(self, *args, **kwargs):
+        super(RandDBox, self).__init__(*args, **kwargs)
