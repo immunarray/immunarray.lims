892d4858a57e140fda98f214ffcdb2bd6d8bef9a
immunarray <justin.pitts@immunarray.com>
2017-09-20 16:44:11 -0400

    import fix

--- immunarray/lims/content/rack.py
+++ immunarray/lims/content/rack.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from Interface import Interface
+from zope.interface import Interface
 from immunarray.lims.interfaces.storage import IRack
 from zope.component import adapter
 from zope.interface import implementer
