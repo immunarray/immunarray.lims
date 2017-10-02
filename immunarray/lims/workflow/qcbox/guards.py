327a391d5ff0c7896aaf1abdf5de519037c388e3
immunarray <justin.pitts@immunarray.com>
2017-10-02 15:17:43 -0400

    inital commit commercail box workflow

---
+++ immunarray/lims/workflow/qcbox/guards.py
@@ -0,0 +1,15 @@
+def guard_active(instance):
+    """
+    """
+    return True
+
+def guard_filled(instance):
+    """
+    """
+    return True
+
+def guard_discarded(instance):
+    """
+    """
+    return True
+
