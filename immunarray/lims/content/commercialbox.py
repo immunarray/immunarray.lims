0e22d714f4a7eb01734ef50ad4a2684d1978b1a7
immunarray <justin.pitts@immunarray.com>
2017-10-03 10:16:48 -0400

    formatting updates

--- immunarray/lims/content/commercialbox.py
+++ immunarray/lims/content/commercialbox.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
-from zope.interface import Interface
 from immunarray.lims.interfaces.storage import ICommercialBox
 from zope.component import adapter
+from zope.interface import Interface
 from zope.interface import implementer
 
 from . import BaseContainer
+
 
 @implementer(ICommercialBox)
 @adapter(Interface)
 class CommercialBox(BaseContainer):
     def __init__(self, *args, **kwargs):
         super(CommercialBox, self).__init__(*args, **kwargs)
-
-
 
     @property
     def box_number(self):
