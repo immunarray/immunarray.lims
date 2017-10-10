3a36e525dbe3a3cd1a1c4a99edec2fb04d935a25
immunarray <justin.pitts@immunarray.com>
2017-10-10 10:50:02 -0400

    not needed

--- immunarray/lims/content/commercialbox.py
+++ immunarray/lims/content/commercialbox.py
@@ -12,22 +12,3 @@ from . import BaseContainer
 class CommercialBox(BaseContainer):
     def __init__(self, *args, **kwargs):
         super(CommercialBox, self).__init__(*args, **kwargs)
-
-    # @property
-    # def box_number(self):
-    #     return getattr(self, "_box_number", "")
-    #
-    # @box_number.setter
-    # def box_number(self, value):
-    #     self._box_number = value
-    #     self.setTitle(value + " - " + self.box_type)
-    #
-    # @property
-    # def box_type(self):
-    #     return getattr(self, "_box_type", "")
-    #
-    # @box_type.setter
-    # def box_type(self, value):
-    #     self._box_type = value
-    #     self.setTitle(self.box_number + " - " + value)
-
