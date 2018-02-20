5a01228e2d00c328887d00c1fe39881ef0f410b9
immunarray <justin.pitts@immunarray.com>
2018-02-20 13:49:57 -0500

    randd sample workflow start

---
+++ immunarray/lims/workflow/randdsample/events.py
@@ -0,0 +1,30 @@
+from plone.api.content import transition, get_state
+
+
+def after_consume(instance):
+    """
+    """
+
+
+def after_reject(instance):
+    """
+    """
+
+
+def after_release(instance):
+    """
+    """
+
+
+def after_review(instance):
+    """
+    """
+
+
+def after_use(instance):
+    """
+    """
+
+def after_throw_away(instance):
+    """
+    """
---
+++ immunarray/lims/workflow/randdsample/guards.py
@@ -0,0 +1,34 @@
+def guard_consume(instance):
+    """
+    """
+    return True
+
+
+def guard_reject(instance):
+    """
+    """
+    return True
+
+
+def guard_release(instance):
+    """
+    """
+    return True
+
+
+def guard_review(instance):
+    """
+    """
+    return True
+
+
+def guard_use(instance):
+    """
+    """
+    return True
+
+
+def guard_throw_away(instance):
+    """
+    """
+    return True
