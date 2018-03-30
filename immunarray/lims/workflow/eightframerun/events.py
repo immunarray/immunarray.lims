2aee311d84bd9a2d2d411d51d6f7f437e640bbe9
Campbell <campbell@bikalabs.com>
2018-03-30 12:45:03 +0200

    fix transition action_id: available->make_available

--- immunarray/lims/workflow/eightframerun/events.py
+++ immunarray/lims/workflow/eightframerun/events.py
@@ -1,5 +1,5 @@
 from immunarray.lims.browser.testrun import ObjectInInvalidState
-from plone.api.content import transition, find
+from plone.api.content import find, transition
 from plone.api.exc import InvalidParameterError
 
 
@@ -33,21 +33,13 @@ def after_scanning(instance):
     """
 
 
-def after_abort_run(instance):
-    """
-    """
-
-
-def get_object(uid):
-    brains = find(UID=uid)
-    if brains:
-        return brains[0].getObject()
-
-
 def after_cancel_run(instance):
+    """When cancelling a test run, all objects that were linked here must
+    be freed for use in a future test run.  This should be logged in this
+    test run's attributes, and the widget's view should be printed on the
+    testrun's view page.
     """
-    """
-    action_id = "available"
+    action_id = "make_available"
     transitioned = []
     try:
         for plate in instance.plates:
@@ -61,3 +53,18 @@ def after_cancel_run(instance):
     except InvalidParameterError:  # noinspection PyUnboundLocalVariable
         msg = "Can't invoke '%s' transition on %s" % (action_id, obj)
         raise ObjectInInvalidState(msg)
+
+
+def after_abort_run(instance):
+    """There are only one type of object supporting abort, and that is the
+    Test Run itself.  So, I guess we don't abort any other objects here when
+    the run is aborted.
+    """
+    # XXX: need a way to show aborted links in the UI
+
+
+
+def get_object(uid):
+    brains = find(UID=uid)
+    if brains:
+        return brains[0].getObject()
