95b99640b203991aab789f369a6d8cd270be03f8
Campbell <campbell@bikalabs.com>
2018-04-24 00:53:28 +0200

    Add proxy EightFrameRun(TestRun) to allow existing objects to load from old databases

---
+++ immunarray/lims/content/eightframerun.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from immunarray.lims.content.testrun import TestRun
+
+
+class EightFrameRun(TestRun):
+    """
+    """
---
+++ immunarray/lims/interfaces/eightframerun.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from immunarray.lims.interfaces.testrun import ITestRun
+
+
+class IEightFrameRun(ITestRun):
+    """
+    """
