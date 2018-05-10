27bb5cf6c6e733effc0d656d3b112200798a8309
Campbell <campbell@bikalabs.com>
2018-05-10 20:46:45 +0200

    update fixup

--- fixup.py
+++ fixup.py
@@ -6,6 +6,7 @@ import transaction
 from AccessControl.SecurityManagement import newSecurityManager
 from immunarray.lims.interfaces.aliquot import IAliquot
 from immunarray.lims.interfaces.ichip import IiChip
+from immunarray.lims.interfaces.provider import IProvider
 from immunarray.lims.interfaces.sample import ISample
 from plone.app.contenttypes import permissions
 
@@ -24,5 +25,11 @@ for brain in app.Plone.portal_catalog(
     instance.manage_permission(
         permissions.AddImage, ['LabManager', 'LabClerk'], 0)
     print("%s addImage ['LabManager', 'LabClerk']" % instance)
+
+for brain in app.Plone.portal_catalog(
+        object_provides=IProvider.__identifier__):
+    instance = brain.getObject()
+    instance.test_report_preference = ['SLE Key Standard']
+    print("Set test_report_preference='SLE Key Standard' for %s" % instance.id)
 
 transaction.commit()
